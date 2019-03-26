d275c807506004e0179eb3f83c149f916d4dd580
Sean Anderson <sean@seananderson.ca>
2019-03-26 11:59:31 -0700

    Run styler

--- pbs2dlm/R/make-rmd.R
+++ pbs2dlm/R/make-rmd.R
@@ -7,10 +7,11 @@
 #'
 #' @examples
 #' create_rmd()
-create_rmd <- function(fn = file.path(here::here("report"),
-                                      "description.Rmd")){
-
-  if(file.exists(fn)){
+create_rmd <- function(fn = file.path(
+                         here::here("report"),
+                         "description.Rmd"
+                       )) {
+  if (file.exists(fn)) {
     stop("File '", fn, "' exists. Delete or move before running this function.")
   }
 
@@ -26,34 +27,47 @@ create_rmd <- function(fn = file.path(here::here("report"),
   #' @examples
   #' format_desc(DLMtool::DataDescription, "Data")
   format_desc <- function(df,
-                          obj_name = "Data",
-                          inst_obj_name = tolower(obj_name)){
-
+                            obj_name = "Data",
+                            inst_obj_name = tolower(obj_name)) {
     df <- df %>%
-      mutate(code = paste0("```{r, ",
-                           inst_obj_name, "-", Slot,
-                           ", results = FALSE, echo = TRUE}\n  ",
-                           inst_obj_name, "@", Slot,
-                           "\n```"),
-             Slot = paste0("## ",
-                           Slot,
-                           "\n"),
-             Description = paste0("*",
-                                  Description,
-                                  "*\n"))
-    c(paste0("# **", obj_name, " slot descriptions**\n\n"),
-      paste0("```{r warnings = FALSE}\n  ",
-             inst_obj_name, " <- methods::new('", obj_name, "')\n```\n"),
-      apply(df, 1, function(x) paste0(x, collapse = "\n\n")))
+      mutate(
+        code = paste0(
+          "```{r, ",
+          inst_obj_name, "-", Slot,
+          ", results = FALSE, echo = TRUE}\n  ",
+          inst_obj_name, "@", Slot,
+          "\n```"
+        ),
+        Slot = paste0(
+          "## ",
+          Slot,
+          "\n"
+        ),
+        Description = paste0(
+          "*",
+          Description,
+          "*\n"
+        )
+      )
+    c(
+      paste0("# **", obj_name, " slot descriptions**\n\n"),
+      paste0(
+        "```{r warnings = FALSE}\n  ",
+        inst_obj_name, " <- methods::new('", obj_name, "')\n```\n"
+      ),
+      apply(df, 1, function(x) paste0(x, collapse = "\n\n"))
+    )
   }
 
-  rmd <- c("```{r message = FALSE}\n  library(DLMtool)\n```\n",
-           format_desc(DLMtool::DataDescription, "Data"),
-           format_desc(DLMtool::StockDescription, "Stock"),
-           format_desc(DLMtool::FleetDescription, "Fleet"),
-           format_desc(DLMtool::ObsDescription, "Obs"),
-           format_desc(DLMtool::ImpDescription, "Imp"),
-           format_desc(DLMtool::OMDescription, "OM"))
+  rmd <- c(
+    "```{r message = FALSE}\n  library(DLMtool)\n```\n",
+    format_desc(DLMtool::DataDescription, "Data"),
+    format_desc(DLMtool::StockDescription, "Stock"),
+    format_desc(DLMtool::FleetDescription, "Fleet"),
+    format_desc(DLMtool::ObsDescription, "Obs"),
+    format_desc(DLMtool::ImpDescription, "Imp"),
+    format_desc(DLMtool::OMDescription, "OM")
+  )
 
   conn <- file(fn)
   write(rmd, conn)
