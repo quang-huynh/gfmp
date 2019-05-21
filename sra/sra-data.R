506172dd6eaa2475bee1a4c24707b053e3830556
Sean Anderson <sean@seananderson.ca>
2019-05-21 13:30:33 -0700

    Fixed typo.

--- sra/sra-data.R
+++ sra/sra-data.R
@@ -9,7 +9,7 @@ d_survey <- gfdata::get_survey_samples(species, ssid = c(1, 3, 4, 16))
 
 cal_wchg <- dplyr::filter(d_survey, survey_abbrev == "SYN WCHG") %>%
   pbs2dlm::tidy_cal(yrs = all_years, interval = 5)
-(length_bins <- get_cal_bins(cal, length_bin_interval = 5))
+(length_bins <- get_cal_bins(cal_wchg, length_bin_interval = 5))
 (cal_wchg <- cal_wchg[1, , ])
 
 caa <- dplyr::filter(d_survey, survey_abbrev == "SYN WCHG") %>%
