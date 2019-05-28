094069dfaa2074d78a1c35e15ee538ccfc79957c
Chris Grandin <cgrandin@shaw.ca>
2019-05-28 14:53:26 -0700

    Tile table for probabilities
    Use geom_tile to make the probability ref. pt. table

--- R/plot-probability-table.R
+++ R/plot-probability-table.R
@@ -1,29 +1,33 @@
-#' Summary of MSE object in a colored tile table format
+#' Summary of probabilities of things from the MSE object in a colored tile table format
 #'
 #' @param object object of class MSE
 #' @param ... a list of names of PM methods
-#' @param Refs An optional named list (matching the PM names) with numeric values to override the default `Ref` values. See examples.
-#' @rdname summary-MSE
-plot_probs <- function(object, ..., Refs = NULL){
-  PMlist <- unlist(list(...))
+#' @param refs An optional named list (matching the PM names) with numeric values to
+#'  override the default `Ref` values. See examples in summary for MSE object.
+#'  @param digits How many decimal places to show in the tiles for the values
+#'  @param sort_by the table will be sorted by the first value in pm_list, this is either "increasing" or "decreasing"
+#'  @param value_size font size for the values in the tiles
+plot_probs <- function(object,
+                       ...,
+                       refs = NULL,
+                       digits = 3,
+                       sort_by = "decreasing",
+                       value_size = 3.1){
+  pm_list <- unlist(list(...))
 
-  if(length(PMlist) == 0) PMlist <- c("PNOF", "P50", "AAVY", "LTY")
-  if (class(PMlist) != 'character') stop("Must provide names of PM methods")
-  # check
-  for (X in seq_along(PMlist)){
-    if (!PMlist[X] %in% avail("PM")) stop(PMlist[X], " is not a valid PM method")
+  if(!length(pm_list)) pm_list <- c("PNOF", "P50", "AAVY", "LTY")
+  if(class(pm_list) != 'character') stop("Must provide names of PM methods")
+
+  for(X in seq_along(pm_list)){
+    if (!pm_list[X] %in% avail("PM")) stop(pm_list[X], " is not a valid PM method")
   }
-  storeMean <- vector('list', length(PMlist))
-  storeName <- vector('list', length(PMlist))
-  storeCap <- vector('list', length(PMlist))
-  storeHeading <- vector('list', length(PMlist))
-  storeMP <- vector('list', length(PMlist))
-  for (X in 1:length(PMlist)) {
-    ref <- Refs[[PMlist[X]]]
+  storeMean <- storeName <- storeCap <- storeMP <- list()
+  for (X in 1:length(pm_list)) {
+    ref <- refs[[pm_list[X]]]
     if (is.null(ref)) {
-      runPM <- eval(call(PMlist[X], object))
+      runPM <- eval(call(pm_list[X], object))
     } else {
-      runPM <- eval(call(PMlist[X], object, Ref=ref))
+      runPM <- eval(call(pm_list[X], object, Ref = ref))
     }
     storeMean[[X]] <- runPM@Mean
     storeName[[X]] <- runPM@Name
@@ -33,14 +37,28 @@ plot_probs <- function(object, ..., Refs = NULL){
 
   df <- data.frame('MP' = storeMP[[1]],
                    signif(do.call('cbind', storeMean),2), stringsAsFactors = FALSE)
-  colnames(df)[2:(length(PMlist)+1)] <- PMlist #caps # gsub(" ", "", caps)
+  colnames(df)[2:(length(pm_list) + 1)] <- pm_list
   df <- as_tibble(df)
   df[,-1] <- apply(df[,-1], c(1, 2), function(x){
-    gfutilities::f(x, 2)
+    gfutilities::f(x, digits)
   })
-  j <- as.vector(do.call('rbind', storeCap))
 
-  #j <- paste0("$", colnames, "$")
+  if(sort_by == "decreasing"){
+    df$MP <- factor(df$MP, levels = df$MP[order(df[2] %>% pull())])
+  }else if(sort_by == "increasing"){
+    df$MP <- factor(df$MP, levels = df$MP[rev(order(df[2] %>% pull()))])
+  }else{
+    stop("Error - sort_by must be either 'increasing' or 'decreasing'",
+         call. = FALSE)
+  }
+
+  df <- reshape2::melt(df,
+                        id.vars = "MP",
+                        variable.name = "type",
+                        value.name = "value")
+
+  ## Set up expressions for tick labels
+  j <- as.vector(do.call('rbind', storeCap))
   yrs <- stringr::str_extract(j, "\\(Year.*\\)$")
   yrs <- gsub("Years", "Yrs", yrs)
   yrs <- gsub(" - ", "-", yrs)
@@ -49,12 +67,23 @@ plot_probs <- function(object, ..., Refs = NULL){
   probs <- gsub("MSY", "_{MSY}", probs)
   probs <- gsub("%", "\\\\%", probs)
   probs <- paste0("$", probs, "$")
-  col.names <- c("Managment Proc.", kableExtra::linebreak(paste0(probs, "\n", yrs),
-                                                          align = "c"))
-  align <- c("l", rep("c", length(probs)))
+  probs <- paste0(probs, "  ", yrs)
+  probs <- unlist(lapply(probs, latex2exp::TeX))
 
-  csasdown::csas_table(df,
-                       align = align,
-                       escape = FALSE,
-                       col.names = col.names)
+  g <- ggplot(df, aes(x = type, y = MP)) +
+    geom_tile(aes(fill = value)) +
+    gfplot::theme_pbs() +
+    theme(panel.border=element_blank(),
+          axis.ticks.x = element_blank(),
+          axis.ticks.y = element_blank(),
+          axis.text.x = element_text(face = "bold", size = 10),
+          axis.text.y = element_text(face = "bold", size = 10)) +
+    scale_colour_viridis_d() +
+    guides(fill = FALSE) + xlab("") + ylab("") +
+    geom_text(aes(x = type, label = value),
+              colour = "black",
+              size = value_size, alpha = 1) +
+    scale_x_discrete(labels = parse(text = probs), position = "left")
+
+  g
 }
