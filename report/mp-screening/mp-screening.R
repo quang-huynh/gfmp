c94d5ea07ba64e2d21af7f9037d6c68a200e9793
Sean Anderson <sean@seananderson.ca>
2019-06-04 14:56:14 -0700

    Move screening script

--- report/mp-screening/mp-screening.R
+++ report/mp-screening/mp-screening.R
@@ -5,19 +5,16 @@ library(DLMtool)
 dir.create(here::here("report/mp-screening/om"), showWarnings = FALSE)
 dir.create(here::here("report/mp-screening/mse-generated"), showWarnings = FALSE)
 
-download.file("http://www.datalimitedtoolkit.org/Case_Studies_Table/Pacific_Ocean_Perch_QC_BC_DFO/OM.rdata",
+base_url <- "http://www.datalimitedtoolkit.org/Case_Studies_Table/"
+download.file(paste0(base_url, "Pacific_Ocean_Perch_QC_BC_DFO/OM.rdata"),
   here::here("report/mp-screening/om/pop.rds"))
-
-download.file("http://www.datalimitedtoolkit.org/Case_Studies_Table/Redbanded_Rockfish_BC_DFO/OM.rdata",
+download.file(paste0(base_url, "Redbanded_Rockfish_BC_DFO/OM.rdata"),
   here::here("report/mp-screening/om/redbanded.rds"))
-
-download.file("http://www.datalimitedtoolkit.org/Case_Studies_Table/Rougheye_Rockfish_BC_DFO/OM.rdata",
+download.file(paste0(base_url, "Rougheye_Rockfish_BC_DFO/OM.rdata"),
   here::here("report/mp-screening/om/rougheye.rds"))
-
-download.file("http://www.datalimitedtoolkit.org/Case_Studies_Table/Shortspine_Thornyhead_BC_DFO/OM.rdata",
+download.file(paste0(base_url, "Shortspine_Thornyhead_BC_DFO/OM.rdata"),
   here::here("report/mp-screening/om/shortspine.rds"))
-
-download.file("http://www.datalimitedtoolkit.org/Case_Studies_Table/Yelloweye_Rockfish_BC_DFO/OM.rdata",
+download.file(paste0(base_url, "Yelloweye_Rockfish_BC_DFO/OM.rdata"),
   here::here("report/mp-screening/om/yelloweye.rds"))
 
 pop_om <- readRDS(here::here("report/mp-screening/om/pop.rds"))
@@ -41,14 +38,17 @@ mps_keep <- vapply(all_mps, function(x) any(grepl(x, candidate_mps$mp)),
 mps_keep <- names(mps_keep[mps_keep])
 
 DLMtool::setup(cpus = parallel::detectCores())
-
 for (i in seq_along(oms)) {
-  oms$seed <- 56129 # not sure if doing anything with this version
-  fi <- paste0(folder, names(oms)[i])
+  oms[[i]]@seed <- 42L
+  oms[[i]]@nsim <- 50L
+  fi <- file.path(folder, names(oms)[i], ".rds")
   if (!file.exists(fi)) {
-    mse[[i]] <- runMSE(OM = oms[[i]], MPs = mps, parallel = TRUE)
+    mse[[i]] <- runMSE(OM = oms[[i]], MPs = mps_keep, parallel = TRUE)
     saveRDS(mse[[i]], file = fi)
   } else {
     mse[[i]] <- readRDS(fi)
   }
 }
+snowfall::sfStop()
+
+plot(mse[[1]])
