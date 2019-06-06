165d739d3e21d8620f7fffa38fa1105b30064529
Sean Anderson <sean@seananderson.ca>
2019-06-06 10:49:31 -0700

    Add to radars

--- report/mp-screening/mp-screening.R
+++ report/mp-screening/mp-screening.R
@@ -117,22 +117,23 @@ top_pm <- wide_pm %>%
   group_by(species) %>%
   mutate(P100 = P100 / max(P100), PNOF = PNOF / max(PNOF),
     P40 = P40 / max(P40), LTY = LTY / max(LTY), AAVY = AAVY / max(AAVY)) %>%
-  filter(P40 > 0.90) %>%
-  filter(P100 > 0.75) %>%
+  filter(P40 > 0.8) %>%
+  filter(P100 > 0.5) %>%
+  filter(LTY > 0.5) %>%
   filter(class != "Reference") %>%
   group_by(species) %>%
-  top_n(n = 10L, wt = LTY) %>%
+  top_n(n = 8L, wt = LTY) %>%
   as.data.frame()
-
-# absolute performance:
-top_pm <- wide_pm %>%
-  group_by(species) %>%
-  filter(P40 > 0.90) %>%
-  filter(P100 > 0.50) %>%
-  filter(class != "Reference") %>%
-  group_by(species) %>%
-  top_n(n = 10L, wt = LTY) %>%
-  as.data.frame()
+#
+# # absolute performance:
+# top_pm <- wide_pm %>%
+#   group_by(species) %>%
+#   filter(P40 > 0.90) %>%
+#   filter(P100 > 0.50) %>%
+#   filter(class != "Reference") %>%
+#   group_by(species) %>%
+#   top_n(n = 10L, wt = LTY) %>%
+#   as.data.frame()
 
 top_pm
 sort(table(top_pm$mp))
@@ -167,12 +168,6 @@ plot_pm("P40", "LTY", "AAVY")
 plot_pm("P40", "PNOF", "LTY")
 plot_pm("P100", "PNOF", "LTY")
 
-# filter(pm[[3]], mp %in% top_top_pm_names) %>%
-#   r_plot()
-# r_plot(pm[[4]])
-# r_plot(pm[[5]])
-# plot(mse[[1]])
-
 # https://stackoverflow.com/a/46999174
 calculate_radar <- function(mydf) {
   df <- cbind(mydf[, -1], mydf[,2])
@@ -186,63 +181,84 @@ calculate_radar <- function(mydf) {
 }
 
 calculate_spokes <- function(mydf) {
-  .n <- ncol(mydf) -1
+  .n <- ncol(mydf) - 1
   angles <- seq(from = 0, to = 2 * pi, by = (2 * pi) / (.n))
   data.frame(x = 0, y = 0, xend = sin(angles[-1]), yend = cos(angles[-1]))
 }
 
-dat <- wide_pm %>%
-  filter(mp %in% top_top_pm_names, species == "pop") %>%
-  select(-1, -2)
+make_radar <- function(.species) {
+  dat <- wide_pm %>%
+    filter(mp %in% top_top_pm_names, species == .species) %>%
+    select(-1, -2)
 
-spokes_data <- calculate_spokes(dat)
-spokes_data$pm <- names(dat[, -1])
-radar_data <- calculate_radar(dat)
-label_data <- data.frame(x = 0, y = c(0.5, 0.75))
+  spokes_data <- calculate_spokes(dat)
+  spokes_data$pm <- names(dat[, -1])
+  radar_data <- calculate_radar(dat)
+  label_data <- data.frame(x = 0, y = c(0.5, 0.75))
 
-radar_data %>%
-  ggplot(aes(x = x, y = y)) +
-  geom_segment(
-    data = spokes_data,
-    aes(x = x, y = y, xend = xend, yend = yend), colour = "grey75", lty = 1
-  ) +
-  # FIXME: do this more elegantly in the data:
-  geom_path(
-    data = rbind(spokes_data, spokes_data[1, ]),
-    aes(x = xend * 0.5, y = yend * 0.5), colour = "grey75", lty = 2
-  ) +
-  geom_path(
-    data = rbind(spokes_data, spokes_data[1, ]),
-    aes(x = xend * 0.75, y = yend * 0.75), colour = "grey75", lty = 2
-  ) +
-  geom_path(
-    data = rbind(spokes_data, spokes_data[1, ]),
-    aes(x = xend * 1, y = yend * 1), colour = "grey75", lty = 2
-  ) +
-  geom_path(aes(colour = as.factor(group)), lwd = 0.8) +
-  coord_equal() +
-  geom_text(data = spokes_data, aes(
-    x = xend * 1.1, y = yend * 1.1,
-    label = pm
-  ), colour = "grey30") +
-  geom_text(
-    data = label_data,
-    aes(x = x, y = y, label = y), colour = "grey50",
-    nudge_y = 0.04, hjust = 0, nudge_x = 0.01
-  ) +
-  gfplot::theme_pbs() +
-  labs(colour = "MP") +
-  scale_color_brewer(palette = "Set3") +
-  theme(
-    axis.line = element_blank(),
-    axis.text.x = element_blank(),
-    axis.text.y = element_blank(),
-    axis.ticks = element_blank(),
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.background = element_blank(),
-    panel.border = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    plot.background = element_blank()
-  )
+  ## for ggrepel labels:
+  # radar_data$pm <- rep(c(names(dat[,-1]), ""), length(unique(dat[,1])))
+  # labs <- filter(radar_data, pm %in% c("AAVY", "PNOF")) %>%
+  #   group_by(group) %>%
+  #   summarize(xx = approx(x = x, y = y, n = 7)$x[6],
+  #     yy = approx(x = x, y = y, n = 7)$y[6])
+
+  radar_data %>%
+    ggplot(aes(x = x, y = y)) +
+    geom_segment(
+      data = spokes_data,
+      aes(x = x, y = y, xend = xend, yend = yend), colour = "grey75", lty = 1
+    ) +
+    # FIXME: do this more elegantly in the data:
+    geom_path(
+      data = rbind(spokes_data, spokes_data[1, ]),
+      aes(x = xend * 0.5, y = yend * 0.5), colour = "grey75", lty = 2
+    ) +
+    geom_path(
+      data = rbind(spokes_data, spokes_data[1, ]),
+      aes(x = xend * 0.75, y = yend * 0.75), colour = "grey75", lty = 2
+    ) +
+    geom_path(
+      data = rbind(spokes_data, spokes_data[1, ]),
+      aes(x = xend * 1, y = yend * 1), colour = "grey75", lty = 2
+    ) +
+    geom_path(aes(colour = as.factor(group)), lwd = 0.8) +
+    coord_equal() +
+    geom_text(data = spokes_data, aes(
+      x = xend * 1.1, y = yend * 1.1,
+      label = pm
+    ), colour = "grey30") +
+    geom_text(
+      data = label_data,
+      aes(x = x, y = y, label = y), colour = "grey50",
+      nudge_y = 0.04, hjust = 0, nudge_x = 0.01
+    ) +
+    gfplot::theme_pbs() +
+    labs(colour = "MP") +
+    # scale_color_viridis_d() +
+    scale_color_brewer(palette = "Set2") +
+    # guides(colour = FALSE) +
+    # ggrepel::geom_text_repel(data = labs,
+    #   aes(x = xx, y = yy, label = group, colour = group),
+    #   nudge_y = 0.1, nudge_x = -0.1) +
+    theme(
+      axis.line = element_blank(),
+      axis.text.x = element_blank(),
+      axis.text.y = element_blank(),
+      axis.ticks = element_blank(),
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.background = element_blank(),
+      panel.border = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      plot.background = element_blank()
+    ) +
+    theme(legend.position="right")
+
+}
+
+out <- lapply(species_names$species, make_radar)
+pdf("report/figure/screening-radar.pdf", width = 17, height = 9)
+cowplot::plot_grid(plotlist = out, labels = species_names$species_full, label_fontface = "plain", hjust = 0, label_x = 0.05)
+dev.off()
