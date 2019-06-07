a05d6f62240f955139ca0fcd211e1d7dfbdaeec6
Chris Grandin <cgrandin@shaw.ca>
2019-06-07 14:43:42 -0700

    Start adding contour lines.

--- R/plots.R
+++ R/plots.R
@@ -2,20 +2,37 @@ plot_contours <- function(object,
                           yend = max(object@proyears),
                           dontshow_mp = NULL,
                           show_ref_pt_lines = FALSE,
-                          bins = 5){
+                          bins = 5,
+                          probs = c(0.025, 0.5, 0.975)){
 
-  ffmsy <- short_mse@F_FMSY[,,yend]
-  bbmsy <- short_mse@B_BMSY[,,yend]
+  ffmsy <- object@F_FMSY[,,yend]
+  bbmsy <- object@B_BMSY[,,yend]
   ffmsy <- reshape2::melt(ffmsy) %>%
     dplyr::rename(iter = Var1, mp = Var2, ffmsy = value)
   bbmsy <- reshape2::melt(bbmsy) %>%
     dplyr::rename(iter = Var1, mp = Var2, bbmsy = value)
   d <- dplyr::inner_join(ffmsy, bbmsy)
-  dn <- data.frame(mp = seq_along(short_mse@MPs), mp_name = short_mse@MPs)
+  dn <- data.frame(mp = seq_along(object@MPs), mp_name = object@MPs)
   d <- dplyr::left_join(d, dn) %>%
     dplyr::filter(!mp_name %in% dontshow_mp)
-  g <- ggplot(d, aes(bbmsy, ffmsy)) +
-    geom_density_2d(aes(colour = ..level..), bins = bins) +
+
+  j <- split(d, d$mp)
+  jj <- lapply(seq_along(j), function(x){
+    dens <- MASS::kde2d(j[[x]]$bbmsy,
+                        j[[x]]$ffmsy,
+                        n = 100,
+                        lims = c(range(j[[x]]$bbmsy),
+                                 range(j[[x]]$ffmsy)))
+    contourLines(dens$x, dens$y, dens$z, levels = probs)[[1]]
+  })
+  ## TODO: Add these lines to the plot
+## jj is length 11, each of those is length 3, $x, $y, and $level representing the contour lines values.
+##
+browser()
+
+    g <- ggplot(d, aes(bbmsy, ffmsy)) +
+    stat_density_2d(data = jjj) +
+    #geom_density_2d(aes(colour = ..level..), bins = bins) +
     # Cannot use breaks in geom_density_2d - Using breaks like in the following has no effect
     #stat_density2d(aes(colour = ..level..), breaks = c(.025, .5, .975), bins = bins, geom = "contour") +
     # Doing the following gives different contours than geom_density_2d but the different breaks produce the same results
