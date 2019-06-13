4dfd01704952894bd4d6e5803b73d4728950ed14
Robyn Forrest <robynforrest@gmail.com>
2019-06-13 11:24:58 -0700

    Updates to SRA-data-pcod.R

--- sra/sra-data-pcod.R
+++ sra/sra-data-pcod.R
@@ -11,7 +11,7 @@ starting_year <- 1956
 ending_year <- 2018
 all_years <- seq(starting_year, ending_year)
 
-#get the OM
+#get the OM -- this file is made using om-specification-pcod.Rmd
 ompc <- readRDS(here::here("generated-data", "pcod-om.rds"))
 
 #Get the data (Areas 5ABCD only)
@@ -139,4 +139,91 @@ pcod_data_summary$mean_length <- mean_lengthpc
 
 #Write to file
 saveRDS(pcod_data_summary, file = here::here("generated-data", "pcod-data-summary.rds"))
+
+######
+# Start Quang's analysis here
+#devtools::install_github('tcarruth/MSEtool')
+library(MSEtool); library(dplyr)
+pcod_data_summary <- readRDS(here::here("generated-data", "pcod-data-summary.rds"))
+pcod_om <- readRDS(here::here("generated-data", "pcod-om.rds"))
+
+# plot CAA - combines QCS and HS surveys
+plot_composition(1956:2018, obs = pcod_data_summary$caa)
+
+# plot CAL
+plot_composition(1956:2018, obs = pcod_data_summary$cal_hsmas, CAL_bins = colnames(pcod_data_summary$cal_hsmas) %>% as.numeric())
+plot_composition(1956:2018, obs = pcod_data_summary$cal_hss, CAL_bins = colnames(pcod_data_summary$cal_hss) %>% as.numeric())
+plot_composition(1956:2018, obs = pcod_data_summary$cal_qcss, CAL_bins = colnames(pcod_data_summary$cal_qcss) %>% as.numeric())
+
+plot_composition(1956:2018, obs = pcod_data_summary$cal_com, CAL_bins = colnames(pcod_data_summary$cal_com) %>% as.numeric())
+
+CAL_sur <- pcod_data_summary$cal_hss
+CAL_sur[, 1:18] <- CAL_sur[, 1:18] + pcod_data_summary$cal_qcss
+
+
+CAL <- pcod_data_summary$cal_com
+CAL[1:41, ] <- NA # Do not use commerical CAL prior to 1996 because they lack discards of smaller fish
+
+Chist <- pcod_data_summary$catch$catch_t
+
+indices <- reshape2::dcast(pcod_data_summary$indices, year ~ survey_abbrev, value.var = "biomass") %>%
+  dplyr::right_join(data.frame(year = 1956:2017))
+
+#RF Added commercial CPUE
+#Join the commercial cpue index to the survey indices
+indices  <- dplyr::full_join(indices, pcod_data_summary$comcpue)
+#remove the CV column for commercial CPUE
+indices <- indices[,-ncol(indices)]
+
+Index <- indices[, -1] %>% as.matrix() %>% rbind(rep(NA, 5))
+
+pcod_om@nsim <- 50L
+pcod_om@Linfsd <- pcod_om@Msd <- pcod_om@Ksd <- c(0, 0)
+
+##### No comps - SRA becomes a de facto delay-difference model
+#pcod_no_comps_SRA <- MSEtool:::SRA_scope(pcod_om, Chist = Chist, Index = Index, I_type = rep(1, 5),cores = 10, report = TRUE)
+pcod_no_comps_SRA <- scope(pcod_om, Chist = Chist, Index = Index, I_type = rep(1, 5),cores = 10, report = TRUE)
+trsaveRDS(pcod_no_comps_SRA$OM, file = here::here("sra/pcod_no_comps_om.rds"))
+saveRDS(pcod_no_comps_SRA$report, file = here::here("sra/pcod_no_comps_SRA_report.rds"))
+
+pcod_no_comps_om <- readRDS("sra/pcod_no_comps_om.rds")
+pcod_no_comps_SRA_report <- readRDS("sra/pcod_no_comps_SRA_report.rds")
+
+MSEtool:::plot_SRA_scope(pcod_no_comps_om, Chist = matrix(Chist, ncol = 1), Index = Index,
+                         report = pcod_no_comps_SRA_report, Year = 1956:2018)
+
+# Use catch, index, and commercial length comps, downweighting the length comps
+# pcod_cal_com_SRA <- MSEtool:::SRA_scope(pcod_om, Chist = Chist, Index = Index, I_type = rep(1, 5),
+#                               CAL = CAL, length_bin = colnames(CAL) %>% as.numeric(), LWT = list(CAL = 0.05),
+#                               cores = 10, report = TRUE)
+# saveRDS(pcod_cal_com_SRA$OM, file = "sra/pcod_cal_com_om.rds")
+# saveRDS(pcod_cal_com_SRA$report, file = "sra/pcod_cal_com_SRA_report.rds")
+#
+# pcod_cal_com_om <- readRDS("sra/pcod_cal_com_om.rds")
+# pcod_cal_com_SRA_report <- readRDS("sra/pcod_cal_com_SRA_report.rds")
+#
+# pcod_cal_com_om@cpars$Find[pcod_cal_com_om@cpars$Find > 1] <- 1 # There are some very high F's, so set max. F = 1
+#
+# MSEtool:::plot_SRA_scope(pcod_cal_com_om, Chist = matrix(Chist, ncol = 1), Index = Index, CAL = array(CAL, c(dim(CAL), 1)),
+#                          report = pcod_cal_com_SRA_report, Year = 1956:2018)
+#
+# #### Use catch, index, and combined age/length comps from Hecate Straight and QCS surveys
+# pcod_cal_sur_SRA <- MSEtool:::SRA_scope(pcod_om, Chist = Chist, Index = Index, I_type = rep(1, 5),
+#                               CAA = pcod_data_summary$caa,
+#                               CAL = CAL_sur, length_bin = colnames(CAL_sur) %>% as.numeric(), LWT = list(CAA = 0.1, CAL = 0.1),
+#                               cores = 10, report = TRUE)
+# saveRDS(pcod_cal_sur_SRA$OM, file = "sra/pcod_cal_sur_om.rds")
+# saveRDS(pcod_cal_sur_SRA$report, file = "sra/pcod_cal_sur_SRA_report.rds")
+#
+# pcod_cal_sur_om <- readRDS("sra/pcod_cal_sur_om.rds")
+# pcod_cal_sur_SRA_report <- readRDS("sra/pcod_cal_sur_SRA_report.rds")
+#
+# pcod_cal_sur_om@cpars$Find[pcod_cal_sur_om@cpars$Find > 1] <- 1 # There are some very high F's, so set max. F = 1
+#
+# MSEtool:::plot_SRA_scope(pcod_cal_sur_om, Chist = matrix(Chist, ncol = 1), Index = Index, CAL = array(CAL_sur, c(dim(CAL_sur), 1)),
+#                          CAA = array(pcod_data_summary$caa, c(dim(pcod_data_summary$caa), 1)),
+#                          report = pcod_cal_sur_SRA_report, Year = 1956:2018)
+
+
+
 
