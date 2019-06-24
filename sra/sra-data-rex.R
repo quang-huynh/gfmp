51c8d66149a7a552b23defaf1a2c24b4ce644c91
Chris Grandin <cgrandin@shaw.ca>
2019-06-24 13:45:12 -0700

    Make sra-data-rex.R work (mostly - no OM yet)

--- sra/sra-data-rex.R
+++ sra/sra-data-rex.R
@@ -23,35 +23,37 @@ rex_om@Perr
 # this is a rough guess as a composite across many indexes; each index has its own CV:
 rex_om@Iobs <- c(0.4, 0.6)
 
-make_raker_cal <- function(dat, survey, length_bin = 5) {
-  dat <- filter(dat, survey_abbrev == survey)
-  cal <- gfdlm::tidy_cal(dat, yrs = all_years, interval = length_bin)
+make_cal <- function(dat,
+                     survey,
+                     yrs,
+                     length_bin = 5) {
+  cal <- dat %>%
+    filter(survey_abbrev == survey) %>%
+    gfdlm::tidy_cal(yrs = yrs, interval = length_bin)
+
   length_bins <- gfdlm::get_cal_bins(cal, length_bin_interval = length_bin)
   list(cal = cal[1, , ], length_bins = length_bins)
 }
 
-cal_wchg <- make_raker_cal(drex$survey_samples, "SYN WCHG")
-cal_qcs <- make_raker_cal(drex$survey_samples, "SYN QCS")
-cal_wcvi <- make_raker_cal(drex$survey_samples, "SYN WCVI")
+cal_wchg <- make_cal(drex$survey_samples, "SYN WCHG", yrs = all_years)
+cal_qcs <- make_cal(drex$survey_samples, "SYN QCS", yrs = all_years)
+cal_wcvi <- make_cal(drex$survey_samples, "SYN WCVI", yrs = all_years)
 
-caa_wchg <- dplyr::filter(drex$survey_samples, survey_abbrev == "SYN WCHG") %>%
-  gfdlm::tidy_caa(yrs = all_years)
-caa_wchg[1, , ]
+# caa_wchg <- filter(drex$survey_samples, survey_abbrev == "SYN WCHG") %>%
+#   gfdlm::tidy_caa(yrs = all_years)
+# caa_wchg[1, , ]
+#
+# caa_qcs <- filter(drex$survey_samples, survey_abbrev == "SYN QCS") %>%
+#   gfdlm::tidy_caa(yrs = all_years)
+# caa_qcs[1, , ]
 
-caa_qcs <- dplyr::filter(drex$survey_samples, survey_abbrev == "SYN QCS") %>%
-  gfdlm::tidy_caa(yrs = all_years)
-caa_qcs[1, , ]
-
-mean_length <- dplyr::filter(drex$survey_samples, survey_abbrev == "SYN WCHG") %>%
+mean_length <- filter(drex$survey_samples, survey_abbrev == "SYN WCHG") %>%
   gfdlm::tidy_mean_length() %>%
-  dplyr::filter(n > 10, year <= ending_year, year >= starting_year) %>%
+  filter(n > 10, year <= ending_year, year >= starting_year) %>%
   right_join(tibble(year = all_years), by = "year") %>%
   pull(mean_length)
 
 mean_length
-
-# note that we be should correcting catch between 1990 and 1995.
-# For now just do that by dividing the catch in those years by 3:
 
 # bottom trawl catch:
 if ("catch" %in% names(drex)) {
@@ -61,17 +63,16 @@ if ("catch" %in% names(drex)) {
     group_by(year) %>%
     summarize(value = sum(value)) %>%
     right_join(tibble(year = all_years), by = "year") %>%
-    mutate(value = ifelse(year >= 1990 & year <= 1995, value / 3, value)) %>%
+    #mutate(value = ifelse(year >= 1990 & year <= 1995, value / 3, value)) %>%
     pull(value)
-  saveRDS(catch, file = here::here("generated-data", "shortraker-catch.rds"))
+  saveRDS(catch, file = here::here("generated-data", "rex-catch.rds"))
 } else {
-  catch <- readRDS(here::here("generated-data", "shortraker-catch.rds"))
+  catch <- readRDS(here::here("generated-data", "rex-catch.rds"))
 }
-
 catch
 plot(all_years, catch, type = "o")
 
-# hook and line catch:
+# hook and line catch for Rex is negligible:
 if ("catch" %in% names(drex)) {
   catch_hl <- drex$catch %>%
     filter(gear == "HOOK AND LINE") %>%
@@ -79,20 +80,17 @@ if ("catch" %in% names(drex)) {
     group_by(year) %>%
     summarize(value = sum(value)) %>%
     right_join(tibble(year = all_years), by = "year") %>%
-    mutate(value = ifelse(year >= 1990 & year <= 1995, value / 3, value)) %>%
+    #mutate(value = ifelse(year >= 1990 & year <= 1995, value / 3, value)) %>%
     pull(value)
-  saveRDS(catch_hl, file = here::here("generated-data", "shortraker-catch-hl.rds"))
+  saveRDS(catch_hl, file = here::here("generated-data", "rex-catch-hl.rds"))
 } else {
-  catch_hl <- readRDS(here::here("generated-data", "shortraker-catch-hl.rds"))
+  catch_hl <- readRDS(here::here("generated-data", "rex-catch-hl.rds"))
 }
-
 catch_hl
 plot(all_years, catch_hl, type = "o")
 
 # catch per unit effort from the trawl fleet only for now:
-cpue <- read.csv(here::here("generated-data", "shortraker-cpue.csv"))
-
-# we are working on something similar for the hook and line fleet
+#cpue <- read.csv(here::here("generated-data", "shortraker-cpue.csv"))
 
 indexes <- gfplot::tidy_survey_index(drex$survey_index) %>%
   filter(survey_abbrev %in% c("SYN WCHG", "SYN QCS", "SYN WCVI", "IPHC FISS")) %>%
