f44d0ec68f35563ef786ba90e0b0a37dda85a201
Sean Anderson <sean@seananderson.ca>
2019-11-12 17:10:07 -0800

    Clean up Rex SRA

--- sra/sra-data-rex.R
+++ sra/sra-data-rex.R
@@ -27,15 +27,12 @@ rex_om@nyears
 rex_om@maxage
 stopifnot(identical(rex_om@nyears, length(all_years)))
 
-# from WCVI:
-# rex_om@Iobs <- c(0.09, 0.09) # specified in `MSEtool::SRA_scope(I_sd = ...)`
-
 make_cal <- function(dat, survey, yrs, length_bin = 5) {
   cal <- dat %>%
     dplyr::filter(survey_abbrev == survey) %>%
     gfdlm::tidy_cal(yrs = yrs, interval = length_bin)
 
-  length_bins <- gfdlm::get_cal_bins(cal, length_bin_interval = length_bin)
+  length_bins <- gfdlm:::get_cal_bins(cal, length_bin_interval = length_bin)
   list(cal = cal[1, , ], length_bins = length_bins)
 }
 
@@ -61,10 +58,10 @@ if ("catch" %in% names(drex)) {
   catch <- readRDS(here::here("generated-data", "rex-catch.rds"))
 }
 catch
-# plot(all_years, catch, type = "o")
+plot(all_years, catch, type = "o")
 
 # catch per unit effort from the trawl fleet only:
-# cpue <- read.csv(here::here("generated-data", rex-cpue.csv"))
+# cpue <- read.csv(here::here("generated-data", "rex-cpue.csv"))
 
 indexes <- drex$survey_index %>%
   dplyr::filter(survey_abbrev %in% c("SYN WCVI")) %>%
@@ -75,13 +72,13 @@ indexes <- drex$survey_index %>%
   as.matrix()
 
 indexes
-# plot(all_years, indexes[, 1L])
-# plot(all_years, indexes[, 2L])
+plot(all_years, indexes[, 1L])
+plot(all_years, indexes[, 2L], type = "o")
 #
-# MSEtool::plot_composition(all_years,
-#   obs = cal_wcvi$cal,
-#   CAL_bins = cal_wcvi$length_bins
-# )
+MSEtool::plot_composition(all_years,
+  obs = cal_wcvi$cal,
+  CAL_bins = cal_wcvi$length_bins
+)
 #
 # # rex_om@nsim <- 8L
 # rex_sra1 <- MSEtool:::SRA_scope(rex_om,
@@ -109,14 +106,19 @@ I_sd
 rex_om@L5
 rex_om@LFS
 
-rex_om@nsim <- 200
+rex_om@nsim <- 100
 cores <- floor(parallel::detectCores() / 1)
+# cores <- floor(1)
 
-rex_sra2 <- MSEtool:::SRA_scope(rex_om,
-  Chist = catch, Index = indexes[, 1L], integrate = FALSE,
-  I_sd = I_sd, I_type = 1L, cores = cores, report = TRUE
+library(MSEtool)
+rex_sra2 <- MSEtool::SRA_scope(rex_om,
+  # CAL = cal_wcvi$cal, length_bin = cal_wcvi$length_bins,
+  Chist = catch, Index = indexes[, 1], integrate = FALSE,
+  I_sd = I_sd, I_type = "B", cores = cores,
+  drop_nonconv = TRUE
 )
 
+plot(rex_sra2)
 # hist(rex_sra2$OM@cpars$D)
 # matplot(t(rex_sra2$OM@cpars$Perr_y), type = "l", lty = 1, col = "#00000040")
 # hist(rex_sra2$OM@cpars$AC)
