9fbbad6981d86c82ad00cfd8860a0f4158fa847a
Sean Anderson <sean@seananderson.ca>
2019-12-02 14:58:28 -0800

    Continue tidying up running of rex mse

--- analysis/rex/03-rex-run-mse.R
+++ analysis/rex/03-rex-run-mse.R
@@ -16,7 +16,6 @@ PM <- c("LT P40", "LT P80", "STY", "LTY", "AAVY", "PNOF")
 omrex_sra <- readRDS(here::here("generated-data", "rex-sra.rds"))
 omrex <- omrex_sra@OM
 omrex@nsim <- 48
-omrex@TACSD <- c(0, 0)
 omrex@interval <- 2
 
 # rex_historical <- runMSE(omrex, Hist = TRUE, parallel = FALSE)
@@ -34,28 +33,27 @@ if (!file.exists(file_name)) {
 
 rex_probs <- gfdlm::get_probs(rex_mse, PM)
 gfdlm::plot_probs(rex_probs)
+ggsave("report/figure/rex-pm-table-base.png", width = 4.25, height = 7)
 
-reference_mp <- c("NFref", "FMSYref")
+reference_mp <- c("FMSYref75", "NFref", "FMSYref")
 rex_satisficed <- dplyr::filter(rex_probs, `LT P40` > 0.9, STY > 0.75) %>%
-  arrange(-`LT P40`) %>%
-  pull(MP)
+  arrange(-`LT P40`) %>% pull(MP)
 rex_satisficed <- rex_satisficed[!rex_satisficed %in% reference_mp]
 rex_satisficed_ref <- union(rex_satisficed, reference_mp)
 
 rex_mse_sub <- DLMtool::Sub(rex_mse, MPs = rex_satisficed)
 rex_mse_sub_ref <- DLMtool::Sub(rex_mse, MPs = rex_satisficed_ref)
-rex_satisficed_df <- left_join(tibble(mp = rex_satisficed), mp, by = "mp")
 
 g1 <- gfdlm::plot_projection_ts(rex_mse_sub_ref, type = c("SSB", "FM")) +
   coord_cartesian(expand = FALSE, ylim = c(0, 4.5)) +
   scale_y_continuous(breaks = c(1, 2, 3, 4)) +
   theme(strip.text.y = element_blank())
 
-catch <- readRDS(here::here("generated-data", "rex-catch.rds"))
-g2 <- gfdlm::plot_projection_ts(rex_mse_sub_ref, type = c("C"), clip_ylim = 1.3, n_samples = 3) +
-  theme(axis.text.y = ggplot2::element_blank(), axis.ticks.y = ggplot2::element_blank()) +
-  theme(axis.title.y = element_blank()) +
-  geom_hline(yintercept = mean(catch[23:23]), lty = 2, alpha = 0.4)
+g2 <- gfdlm::plot_projection_ts(rex_mse_sub_ref, type = "C", clip_ylim = 1.3,
+  catch_reference = 1) +
+  theme(axis.text.y = ggplot2::element_blank(),
+    axis.ticks.y = ggplot2::element_blank(),
+    axis.title.y = element_blank())
 
 g <- cowplot::plot_grid(g1, g2, rel_widths = c(2, 1), align = "h")
 ggsave("report/figure/rex-projections-base.png", width = 11, height = 12)
@@ -71,15 +69,13 @@ g <- filter(mp, type == "Constant catch") %>%
   pull(mp) %>%
   DLMtool::Sub(rex_mse, .) %>%
   gfdlm::spider(pm_list = PM, palette = "Set2", lwd = 1.0) +
-  scale_color_manual(values = cols) +
-  labs(colour = "MP")
+  scale_color_manual(values = cols)
 ggsave("report/figure/rex-spider-cc-base.png", width = 6, height = 6)
 
 g <- rex_satisficed %>%
   DLMtool::Sub(rex_mse, .) %>%
   gfdlm::spider(pm_list = PM, palette = "Set2", lwd = 1.0) +
   scale_color_viridis_d() +
-  # scale_color_manual(values =
-  #     c(RColorBrewer::brewer.pal(length(mp_unique), "Set2"), "grey30", "grey65")) +
-  labs(colour = "MP")
+  scale_color_manual(values =
+      c(RColorBrewer::brewer.pal(length(rex_satisficed), "Set2"), "grey50"))
 ggsave("report/figure/rex-spider-base.png", width = 6, height = 6)
