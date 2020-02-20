53ec63ceb3573c5b31c80a9d72ecd9a8ebb9481e
Sean Anderson <sean@seananderson.ca>
2020-02-19 23:37:03 -0800

    Update rex mse runs to match latest
    In flux...

--- analysis/rex/03-run-mse.R
+++ analysis/rex/03-run-mse.R
@@ -1,3 +1,6 @@
+# 2020-02-19
+# DO NOTE USE ME!
+# See 05-plot-all.R for now.
 library("DLMtool")
 library("MSEtool")
 library("dplyr")
@@ -5,16 +8,17 @@ library("purrr")
 library("ggplot2")
 library("gfdlm")
 library("here")
+library("assertthat")
 
 # Settings --------------------------------------------------------------------
 
 sp <- "rex" # Species: used in filenames
 cores <- floor(parallel::detectCores() / 2L)
-sc <- readRDS("generated-data/rex-scenarios.rds")
+sc <- readRDS(here("generated-data/rex-scenarios.rds"))
 sc # look good?
-nsim <- 200L
+nsim <- 50
 interval <- 2L
-base_om <- "ceq100" # affects some default plots
+base_om <- "ceq100" # affects "failed" MP example trajectory plot
 mp <- suppressMessages(readr::read_csv(here("analysis", "rex", "mp.txt"), comment = "#"))
 as.data.frame(mp) # look good?
 reference_mp <- c("FMSYref75", "NFref", "FMSYref")
@@ -30,13 +34,18 @@ this_year <- 2018
 
 # Set up PMs ------------------------------------------------------------------
 
+catch <- readRDS(here("generated-data", "rex-catch.rds"))
+yrs <- length(catch)
+ref_catch <- mean(catch[(yrs-5+1):yrs])
+ref_aadc <- gfdlm:::get_aadc(catch[(yrs-10+1):yrs])
+
 `LT LRP` <- gfdlm::pm_factory("SBMSY", 0.4, c(36, 50))
 `LT USR` <- gfdlm::pm_factory("SBMSY", 0.8, c(36, 50))
-`FMSY` <- DLMtool::PNOF
-`AAVC` <- DLMtool::AAVY
-`STC` <- gfdlm::pm_factory("LTY", 0.5, c(1, 10))
-`LTC` <- gfdlm::pm_factory("LTY", 0.5, c(36, 50))
-PM <- c("LT LRP", "LT USR", "FMSY", "STC", "LTC", "AAVC")
+FMSY <- DLMtool::PNOF
+STC <- gfdlm::pm_factory("LTY", 1, c(1, 10))
+LTC <- gfdlm::pm_factory("LTY", 1, c(36, 50))
+AADC <- gfdlm::pm_factory("AADC", ref_aadc, c(1, 50))
+PM <- c("LT LRP", "LT USR", "FMSY", "STC", "LTC", "AADC")
 
 # Set up and checks -----------------------------------------------------------
 
@@ -72,27 +81,38 @@ om <- map(scenarios, ~ {
   om@interval <- interval
   om
 })
-names(om) <- scenarios
 
 # Fit MPs to all OMs ----------------------------------------------------------
 
-# .om <- om[[1]]
-# .om@nsim <- 5
-# load_all("../gfdlm/")
-# .mp <- add_SP_prior(.SP4010_gf, r_prior = c(0.3, 0.1))
-# .mp <- add_SP_prior(.SP4010_gf, r_prior = c(0.3, 0.1))
-# .mp <- add_SP_prior(.SP4010_gf, r_prior = c(0.3, 0.1))
-# .mp <- use_AddInd(IDX)
-# m <- runMSE(OM =.om, MPs = ".mp", parallel = FALSE)
+.SP6040_prior <- add_SP_prior(.SP6040_gf, r_prior = c(0.3, 0.1))
+.SP8040_prior <- add_SP_prior(.SP8040_gf, r_prior = c(0.3, 0.1))
+.SP4010_prior <- add_SP_prior(.SP4010_gf, r_prior = c(0.3, 0.1))
 
+missing <- mp$mp[!map_lgl(mp$mp, exists)]
+assert_that(length(missing) == 0,
+  msg = glue::glue(
+    "{paste(missing, collapse = ', ')} do not exist in the current environment."
+  ))
+
+# testing:
+# .mp <- c(
+#   "CC_hist20", "CC1.2", "CC1.1", "CC1.0", "CC0.9", "CC0.8", "CC0.7", "CC0.6",
+#   ".Iratio2",
+#   ".GB_slope6_0.66", ".GB_slope6_1", ".GB_slope8_0.66", ".GB_slope8_1",
+#   ".Islope0.2_80", ".Islope0.2_100", ".Islope0.4_80", ".Islope0.4_100",
+#   ".IDX", ".IDX_smooth",
+#   ".IT10_hist", ".IT5_hist",
+#   ".Itarget_base", ".Itarget_w0.8", ".Itarget_x0.2", ".Itarget_x0.8", ".Itarget_d1.2", ".Itarget_d0.8",
+#   ".ITM_hist",
+#   ".SP4010_prior", ".SP6040_prior", ".SP8040_prior",
+#   "NFref", "FMSYref", "FMSYref75"
+# )
 
 fit_scenario <- function(scenario) {
   file_name <- here("generated-data", paste0(sp, "-mse-", scenario, ".rds"))
   if (!file.exists(file_name)) {
     message("Running closed-loop-simulation for ", scenario, " OM")
-    DLMtool::setup(cpus = cores)
     mse <- runMSE(OM = om[[scenario]], MPs = mp$mp, parallel = TRUE)
-    snowfall::sfStop()
     saveRDS(mse, file = file_name)
   } else {
     message("Loading closed-loop-simulation for ", scenario, " OM")
@@ -100,34 +120,40 @@ fit_scenario <- function(scenario) {
   }
   mse
 }
+DLMtool::setup(cpus = cores)
 mse <- map(scenarios, fit_scenario)
-names(mse) <- scenarios
+snowfall::sfStop()
+for (i in seq_along(mse)) mse[[i]]@OM$RefY <- ref_catch * 1
 
 # Satisficing -----------------------------------------------------------------
 
 pm_df_list <- map(mse[scenarios_ref], ~ gfdlm::get_probs(.x, PM))
 pm_df_list_rob <- map(mse[scenarios_rob], ~ gfdlm::get_probs(.x, PM))
 pm_df <- bind_rows(pm_df_list, .id = "scenario")
-saveRDS(pm_df, file = here::here("generated-data/rex-pm-all.rds"))
 pm_avg <- group_by(pm_df, MP) %>% summarise_if(is.numeric, mean)
 pm_min <- group_by(pm_df, MP) %>% summarise_if(is.numeric, min)
+saveRDS(pm_df_list, file = here("generated-data", "rex-pm-all.rds"))
 
-mp_sat <- dplyr::filter(pm_min, `LT LRP` > LT_LRP_thresh, `STC` > STC_thresh) %>%
-  arrange(-`LT LRP`, -`LT LRP`, -`LT USR`, -`STC`, -`LTC`, -AAVC) %>%
+# plot_tigure_facet(pm_df_list)
+plot_tigure(pm_avg, satisficed = c("LT LRP" = 0.8, "STC" = 0.5))
+plot_tigure(pm_min)
+
+mp_sat <- dplyr::filter(pm_avg, `LT LRP` > 0.8, `STC` > 0.6) %>%
+  # arrange(-`LT LRP`, -`LT LRP`, -`LT USR`, -`STC`, -`LTC`) %>%
   pull(MP)
 mp_sat <- mp_sat[!mp_sat %in% reference_mp]
 mp_sat
-stopifnot(length(mp_sat) > 1)
-stopifnot(length(mp_sat) <= 8) # for RColorBrewer::brewer.pal() "Set2"
+stopifnot(length(mp_sat) >= 1)
+stopifnot(length(mp_sat) <= 8) # for RColorBrewer::brewer.pal()
 mp_sat_with_ref <- union(mp_sat, reference_mp)
 mp_not_sat <- mp$mp[!mp$mp %in% mp_sat_with_ref]
 stopifnot(length(mp_not_sat) > 1)
 
-mse_sat <- purrr::map(scenarios, ~ DLMtool::Sub(mse[[.x]], MPs = mp_sat))
-mse_sat_with_ref <- purrr::map(scenarios_ref, ~ DLMtool::Sub(mse[[.x]], MPs = mp_sat_with_ref))
+mse_sat <- map(scenarios, ~ DLMtool::Sub(mse[[.x]], MPs = mp_sat))
+mse_sat_with_ref <- map(scenarios_ref, ~ DLMtool::Sub(mse[[.x]], MPs = mp_sat_with_ref))
 
 pm_df_list_sat <- map(pm_df_list, dplyr::filter, MP %in% mp_sat)
 pm_df_list_sat_with_ref <- map(pm_df_list, dplyr::filter, MP %in% mp_sat_with_ref)
 
-custom_pal <- c(RColorBrewer::brewer.pal(length(mp_sat), "Set2"), ref_mp_cols) %>%
+custom_pal <- c(RColorBrewer::brewer.pal(8, "Set2")[seq_along(mp_sat)], ref_mp_cols) %>%
   set_names(mp_sat_with_ref)
--- analysis/rex/05-plot-all.R
+++ analysis/rex/05-plot-all.R
@@ -5,6 +5,7 @@ library("purrr")
 library("ggplot2")
 library("gfdlm")
 library("here")
+library("assertthat")
 # load_all("../gfdlm")
 
 # Settings --------------------------------------------------------------------
@@ -13,7 +14,7 @@ sp <- "rex" # Species: used in filenames
 cores <- floor(parallel::detectCores() / 2L)
 sc <- readRDS("generated-data/rex-scenarios.rds")
 sc # look good?
-nsim <- 200L
+nsim <- 50L
 interval <- 2L
 base_om <- "ceq100" # affects some default plots
 mp <- suppressMessages(readr::read_csv(here("analysis", "rex", "mp.txt"), comment = "#"))
@@ -30,13 +31,18 @@ STC_thresh <- 0.7
 
 # Set up PMs ------------------------------------------------------------------
 
+catch <- readRDS(here("generated-data", "rex-catch.rds"))
+yrs <- length(catch)
+ref_catch <- mean(catch[(yrs-5+1):yrs])
+ref_aadc <- gfdlm:::get_aadc(catch[(yrs-10+1):yrs])
+
 `LT LRP` <- gfdlm::pm_factory("SBMSY", 0.4, c(36, 50))
 `LT USR` <- gfdlm::pm_factory("SBMSY", 0.8, c(36, 50))
-`FMSY` <- DLMtool::PNOF
-`AAVC` <- DLMtool::AAVY
-`STC` <- gfdlm::pm_factory("LTY", 0.5, c(1, 10))
-`LTC` <- gfdlm::pm_factory("LTY", 0.5, c(36, 50))
-PM <- c("LT LRP", "LT USR", "FMSY", "STC", "LTC", "AAVC")
+FMSY <- DLMtool::PNOF
+STC <- gfdlm::pm_factory("LTY", 1, c(1, 10))
+LTC <- gfdlm::pm_factory("LTY", 1, c(36, 50))
+AADC <- gfdlm::pm_factory("AADC", ref_aadc, c(1, 50))
+PM <- c("LT LRP", "LT USR", "FMSY", "STC", "LTC", "AADC")
 
 # Set up and checks -----------------------------------------------------------
 
@@ -69,17 +75,38 @@ om <- map(scenarios, ~ {
   om@interval <- interval
   om
 })
-names(om) <- scenarios
 
 # Fit MPs to all OMs ----------------------------------------------------------
+
+.SP6040_prior <- add_SP_prior(.SP6040_gf, r_prior = c(0.3, 0.1))
+.SP8040_prior <- add_SP_prior(.SP8040_gf, r_prior = c(0.3, 0.1))
+.SP4010_prior <- add_SP_prior(.SP4010_gf, r_prior = c(0.3, 0.1))
+
+missing <- mp$mp[!map_lgl(mp$mp, exists)]
+assert_that(length(missing) == 0,
+  msg = glue::glue(
+    "{paste(missing, collapse = ', ')} do not exist in the current environment."
+  ))
+
+# testing:
+# .mp <- c(
+#   "CC_hist20", "CC1.2", "CC1.1", "CC1.0", "CC0.9", "CC0.8", "CC0.7", "CC0.6",
+#   ".Iratio2",
+#   ".GB_slope6_0.66", ".GB_slope6_1", ".GB_slope8_0.66", ".GB_slope8_1",
+#   ".Islope0.2_80", ".Islope0.2_100", ".Islope0.4_80", ".Islope0.4_100",
+#   ".IDX", ".IDX_smooth",
+#   ".IT10_hist", ".IT5_hist",
+#   ".Itarget_base", ".Itarget_w0.8", ".Itarget_x0.2", ".Itarget_x0.8", ".Itarget_d1.2", ".Itarget_d0.8",
+#   ".ITM_hist",
+#   ".SP4010_prior", ".SP6040_prior", ".SP8040_prior",
+#   "NFref", "FMSYref", "FMSYref75"
+# )
 
 fit_scenario <- function(scenario) {
   file_name <- here("generated-data", paste0(sp, "-mse-", scenario, ".rds"))
   if (!file.exists(file_name)) {
     message("Running closed-loop-simulation for ", scenario, " OM")
-    DLMtool::setup(cpus = cores)
     mse <- runMSE(OM = om[[scenario]], MPs = mp$mp, parallel = TRUE)
-    snowfall::sfStop()
     saveRDS(mse, file = file_name)
   } else {
     message("Loading closed-loop-simulation for ", scenario, " OM")
@@ -88,37 +115,42 @@ fit_scenario <- function(scenario) {
   mse@OM$CurrentYr <- om[[1]]@CurrentYr # fixed in latest DLMtool
   mse
 }
+DLMtool::setup(cpus = cores)
 mse <- map(scenarios, fit_scenario)
-names(mse) <- scenarios
+snowfall::sfStop()
+for (i in seq_along(mse)) mse[[i]]@OM$RefY <- ref_catch * 1
 
 # Satisficing -----------------------------------------------------------------
 
 pm_df_list <- map(mse[scenarios_ref], ~ gfdlm::get_probs(.x, PM))
 pm_df_list_rob <- map(mse[scenarios_rob], ~ gfdlm::get_probs(.x, PM))
 pm_df <- bind_rows(pm_df_list, .id = "scenario")
-saveRDS(pm_df, file = here::here("generated-data/rex-pm-all.rds"))
 pm_avg <- group_by(pm_df, MP) %>% summarise_if(is.numeric, mean)
 pm_min <- group_by(pm_df, MP) %>% summarise_if(is.numeric, min)
-#
-mp_sat <- dplyr::filter(pm_min, `LT LRP` > LT_LRP_thresh, `STC` > STC_thresh) %>%
-  arrange(-`LT LRP`, -`LT LRP`, -`LT USR`, -`STC`, -`LTC`, -AAVC) %>%
+saveRDS(pm_df_list, file = here("generated-data", "rex-pm-all.rds"))
+
+plot_tigure(pm_avg, satisficed = c("LT LRP" = 0.75, "STC" = 0.5))
+plot_tigure(pm_min)
+
+mp_sat <- dplyr::filter(pm_avg, `LT LRP` > 0.5, `STC` > 0.3) %>%
+  # arrange(-`LT LRP`, -`LT LRP`, -`LT USR`, -`STC`, -`LTC`, -AAVC) %>%
   pull(MP)
 mp_sat <- mp_sat[!mp_sat %in% reference_mp]
 mp_sat
-stopifnot(length(mp_sat) > 1)
-stopifnot(length(mp_sat) <= 8) # for RColorBrewer::brewer.pal() "Set2"
+stopifnot(length(mp_sat) >= 1)
+stopifnot(length(mp_sat) <= 8) # for RColorBrewer::brewer.pal()
 mp_sat_with_ref <- union(mp_sat, reference_mp)
 mp_not_sat <- mp$mp[!mp$mp %in% mp_sat_with_ref]
 stopifnot(length(mp_not_sat) > 1)
 
-custom_pal <- c(RColorBrewer::brewer.pal(length(mp_sat), "Set2"), ref_mp_cols) %>%
+custom_pal <- c(RColorBrewer::brewer.pal(8, "Set2")[seq_along(mp_sat)], ref_mp_cols) %>%
   set_names(mp_sat_with_ref)
 
 satisficed_criteria <- c("LT LRP" = LT_LRP_thresh, "STC" = STC_thresh)
 
 mp_eg_not_sat <- c(
-  "CC_hist",
-  "CC90",
+  "CC_hist20",
+  "CC1.0",
   ".GB_slope8_0.66",
   ".Islope0.2_80",
   ".ICI2",
@@ -142,12 +174,15 @@ plots <- gfdlm::plot_factory(
   catch_breaks = catch_breaks,
   catch_labels = catch_labels,
   satisficed_criteria = satisficed_criteria,
-  skip_projections = TRUE,
+  skip_projections = FALSE,
   dodge = 0.8
 )
 
-pm_angle <- theme(axis.text.x.bottom = element_text(angle = 90, hjust = 1), panel.grid.major.y = element_line(colour = "grey85"),
-  panel.grid.minor.y = element_line(colour = "grey96"))
+pm_angle <- theme(
+  axis.text.x.bottom = element_text(angle = 90, hjust = 1),
+  panel.grid.major.y = element_line(colour = "grey85"),
+  panel.grid.minor.y = element_line(colour = "grey96")
+)
 plots$dot_refset + pm_angle
 # ggsave("~/Desktop/g1.pdf", width = 11, height = 7)
 plots$dot_refset_avg
