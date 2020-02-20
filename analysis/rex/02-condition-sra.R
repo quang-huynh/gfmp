8d7d8d28c8dd199aec057eb83e0f502f9a6a130f
Sean Anderson <sean@seananderson.ca>
2020-02-20 11:05:45 -0800

    Messed up rex OM

--- analysis/rex/02-condition-sra.R
+++ analysis/rex/02-condition-sra.R
@@ -16,6 +16,8 @@ fig_dir <- here("report", "figure")
 if (!dir.exists(fig_dir)) dir.create(fig_dir)
 
 drex <- readRDS(here("generated-data", "rex-filter-data.rds"))
+d_catch <- readRDS(here::here("generated-data", "rex-catch.rds"))
+drex$catch <- dplyr::filter(d_catch, year <= 2018)
 
 rex_om <- readRDS(here("generated-data", "rex-om.rds"))
 rex_om@M
@@ -42,6 +44,16 @@ rex_om@L5
 rex_om@LFS
 assertthat::assert_that(identical(rex_om@nyears, length(all_years)))
 
+# d_cpue <- readRDS("/Volumes/Extreme-SSD/gfs/report/data-cache/cpue-index-dat.rds")
+# table(lubridate::year(d_cpue$best_date))
+# d_cpue <- dplyr::filter(d_cpue, lubridate::year(best_date) <= 2019)
+# m <- gfsynopsis::fit_cpue_indices(dat = d_cpue,
+#   species = "rex sole", areas = "3[CD]",
+#   center = FALSE,
+#   cache = here::here("generated-data"),
+#   save_model = TRUE)
+# ggplot(m, aes(year, est)) + geom_line() + facet_wrap(~model)
+
 make_cal <- function(dat, survey, yrs, length_bin = 2) {
   cal <- dat %>%
     dplyr::filter(survey_abbrev == survey, sex == 2) %>%
@@ -53,18 +65,12 @@ make_cal <- function(dat, survey, yrs, length_bin = 2) {
 
 cal_wcvi <- make_cal(drex$survey_samples, "SYN WCVI", yrs = all_years)
 
-if ("catch" %in% names(drex)) {
-  catch <- drex$catch %>%
-    gfplot::tidy_catch() %>%
-    group_by(year) %>%
-    summarize(value = sum(value)) %>%
-    right_join(tibble(year = all_years), by = "year") %>%
-    pull(value)
-  saveRDS(catch, file = here::here("generated-data", "rex-catch.rds"))
-} else {
-  catch <- readRDS(here::here("generated-data", "rex-catch.rds"))
-}
-catch
+catch <- drex$catch %>%
+  gfplot::tidy_catch() %>%
+  group_by(year) %>%
+  summarize(value = sum(value)) %>%
+  right_join(tibble(year = all_years), by = "year") %>%
+  pull(value)
 plot(all_years, catch, type = "o")
 
 # catch per unit effort from the trawl fleet only:
@@ -88,7 +94,7 @@ plot(all_years, indexes$trawl_cpue, type = "p")
 #   CAL_bins = cal_wcvi$length_bins
 # )
 
-rex_om@nsim <- 260 # 10 extras in case some don't converge
+rex_om@nsim <- 55 # 10 extras in case some don't converge
 cores <- floor(parallel::detectCores() / 2)
 
 rex_om@Cobs <- c(0, 0)
@@ -99,22 +105,36 @@ rex_om@D <- c(0.3, 0.8) # gets replaced
 
 saveRDS(indexes, file = "generated-data/rex-indexes.rds")
 fit_sra_rex <- function(om, c_eq = 1, ...) {
-  MSEtool::SRA_scope(rex_om,
-    Chist = catch, Index = indexes$biomass,
+  MSEtool::SRA_scope(om,
+    Chist = catch,
     C_eq = c_eq * catch[1],
-    I_sd = indexes$re, I_type = "1", cores = cores,
+    Index = cbind(indexes$biomass, indexes$trawl_cpue),
+    I_sd = cbind(indexes$re, indexes$trawl_sd * 1.5),
+    I_type = c("1", "1"),
+    cores = 1,
     drop_nonconv = TRUE, mean_fit = FALSE, ...
   )
 }
 
-rex_sra_ceq0 <- fit_sra_rex(rex_om, c_eq = 0)
-rex_sra_ceq50 <- fit_sra_rex(rex_om, c_eq = 0.5)
-rex_sra_ceq100 <- fit_sra_rex(rex_om, c_eq = 1)
-rex_sra_ceq200 <- fit_sra_rex(rex_om, c_eq = 2)
+rex_om@nsim <- 52
+rex_om@L5
+rex_om@LFS
+rex_om@L50
+rex_om@Linf
+om <- rex_om
+# om@L5 <- c(30, 30)
+# om@LFS <- c(35, 35)
 
-saveRDS(rex_sra_ceq0, file = here("generated-data", "rex-sra-ceq0.rds"))
-saveRDS(rex_sra_ceq50, file = here("generated-data", "rex-sra-ceq50.rds"))
-saveRDS(rex_sra_ceq100, file = here("generated-data", "rex-sra-ceq100.rds"))
+# Abookire (2006) modeled female rex sole size-at-maturity using a logistic model, obtaining a value for size at 50% maturity of 351.7 mm with a slope of 0.0392 mm-1
+# rex_sra_ceq0 <- fit_sra_rex(rex_om, c_eq = 0)
+# rex_sra_ceq50 <- fit_sra_rex(rex_om, c_eq = 0.5)
+# rex_sra_ceq100 <- fit_sra_rex(rex_om, c_eq = 1)
+rex_sra_ceq200 <- fit_sra_rex(om, c_eq = 2)
+plot(rex_sra_ceq200)
+
+# saveRDS(rex_sra_ceq0, file = here("generated-data", "rex-sra-ceq0.rds"))
+# saveRDS(rex_sra_ceq50, file = here("generated-data", "rex-sra-ceq50.rds"))
+# saveRDS(rex_sra_ceq100, file = here("generated-data", "rex-sra-ceq100.rds"))
 saveRDS(rex_sra_ceq200, file = here("generated-data", "rex-sra-ceq200.rds"))
 
 median(rex_sra_ceq0@OM@cpars$D)
@@ -132,10 +152,6 @@ reshape2::melt(indexes[,c(1, 2, 4)], id.vars = "year") %>%
 rex_sra_ceq200_cpue <- MSEtool::SRA_scope(rex_om,
   data = list(
     Chist = catch,
-    #s_CAL = s_CAL,
-    #ESS = c(50, 50),
-    #length_bin = cal_wcvi$length_bins,
-    #CAL = array(0, c(rex_om@nyears, length(cal_wcvi$length_bins), 1L)),
     Index = cbind(indexes$biomass, indexes$trawl_cpue),
     I_sd = cbind(indexes$re, indexes$trawl_sd * 1.5),
     I_type = c("1", "1"),
@@ -277,3 +293,52 @@ sel %>%
   ylab("Selectivity") + xlab("Length") +
   coord_cartesian(expand = FALSE, ylim = c(-0.01, 1.01))
 ggsave(here::here("report/figure/rex-selectivity.png"), width = 5, height = 3)
+
+# ------------------------------------------------------------------------
+
+# fit_sra_rex <- function(om, c_eq = 1, ...) {
+#   MSEtool::SRA_scope(rex_om,
+#     Chist = catch, Index = indexes$biomass,
+#     C_eq = c_eq * catch[1],
+#     I_sd = indexes$re, I_type = "1", cores = 1,
+#     drop_nonconv = TRUE, mean_fit = FALSE, ...
+#   )
+# }
+# rex_om@nsim <- 18
+# rex_sra_ceq200 <- fit_sra_rex(rex_om, c_eq = 2)
+
+# sra_rex <- purrr::map(sc$scenario, ~ {
+#   readRDS(here("generated-data", paste0("rex-sra-", .x, ".rds")))
+# })
+# names(sra_rex) <- sc$scenario
+
+# sra <- sra_rex[[1]]
+# sra <- rex_sra_ceq200
+# OM <- sra@OM
+#
+# library(DLMtool)
+# mse <- runMSE(OM, Hist = TRUE, parallel = FALSE)
+#
+#
+#
+# library(MSEtool)
+# om <- DLMtool::testOM
+# om@nsim <- 10
+# set.seed(1)
+# m <- SRA_scope(om,
+#   data = list(Chist = runif(10), Index = runif(10), I_sd = rep(0.1, 10))
+# )
+# fits <- list()
+# fits[[1]] <- m
+# fits[[2]] <- m
+# names(fits) <- c("Sc 1", "Sc 2")
+# oms <- purrr::map(fits, "OM")
+# mse_list <- purrr::map(oms, runMSE, Hist = TRUE, parallel = FALSE)
+#
+# dim(mse_list[[1]]@TSdata$SSB)
+# mse_list[[1]]@Ref$SSBMSY
+
+# make plot_historical_ts() work with:
+# 1. a list of mse objects
+# 2. an optional `ref_pt` argument that pulls a value from `@Ref` and divides
+# the time series by it.
