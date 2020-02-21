f4b8b1f640127aadba519c1fa4c91f02d8ed1856
Sean Anderson <sean@seananderson.ca>
2020-02-20 16:22:30 -0800

    Run new rex MSE and make plots

--- analysis/rex/05-plot-all.R
+++ analysis/rex/05-plot-all.R
@@ -16,24 +16,30 @@ sc <- readRDS("generated-data/rex-scenarios.rds")
 sc # look good?
 nsim <- 50L
 interval <- 2L
-base_om <- "ceq100" # affects some default plots
+base_om <- "ceq150" # affects some default plots
 mp <- suppressMessages(readr::read_csv(here("analysis", "rex", "mp.txt"), comment = "#"))
 as.data.frame(mp) # look good?
 reference_mp <- c("FMSYref75", "NFref", "FMSYref")
 ref_mp_cols <- c("grey45", "grey10", "grey75") %>% set_names(reference_mp)
 #
-catch_breaks <- c(0, 100000, 200000, 300000)
-catch_labels <- c("0", "100", "200", "300")
+catch_breaks <- seq(0, 500000, 100000)
+catch_labels <- seq(0, 500000, 100000)/1000
 
 # Satisficing rules:
-LT_LRP_thresh <- 0.8
-STC_thresh <- 0.7
+# LT_LRP_thresh <- 0.8
+# STC_thresh <- 0.7
 
 # Set up PMs ------------------------------------------------------------------
 
-catch <- readRDS(here("generated-data", "rex-catch.rds"))
+d_catch <- readRDS(here::here("generated-data", "rex-catch2.rds"))
+d_catch <- dplyr::filter(d_catch, year >= 1996, year <= 2019)
+catch <- d_catch %>%
+  gfplot::tidy_catch() %>%
+  group_by(year) %>%
+  summarize(value = sum(value)) %>%
+  pull(value)
+
 yrs <- length(catch)
-ref_catch <- mean(catch[(yrs-5+1):yrs])
 ref_aadc <- gfdlm:::get_aadc(catch[(yrs-10+1):yrs])
 
 `LT LRP` <- gfdlm::pm_factory("SBMSY", 0.4, c(36, 50))
@@ -49,9 +55,9 @@ PM <- c("LT LRP", "LT USR", "FMSY", "STC", "LTC", "AADC")
 stopifnot(all(reference_mp %in% mp$mp))
 fig_dir <- here("report", "figure")
 if (!dir.exists(fig_dir)) dir.create(fig_dir)
-.ggsave <- function(filename, width, height) {
+.ggsave <- function(filename, width, height, ...) {
   ggsave(file.path(fig_dir, paste0(sp, "-", filename, ".png")),
-    width = width, height = height
+    width = width, height = height, ...
   )
 }
 
@@ -73,6 +79,7 @@ om <- map(scenarios, ~ {
   }
   om@nsim <- nsim
   om@interval <- interval
+  om@TACSD <- c(0, 0)
   om
 })
 
@@ -88,20 +95,21 @@ assert_that(length(missing) == 0,
     "{paste(missing, collapse = ', ')} do not exist in the current environment."
   ))
 
-# testing:
+# # testing:
 # .mp <- c(
-#   "CC_hist20", "CC1.2", "CC1.1", "CC1.0", "CC0.9", "CC0.8", "CC0.7", "CC0.6",
-#   ".Iratio2",
-#   ".GB_slope6_0.66", ".GB_slope6_1", ".GB_slope8_0.66", ".GB_slope8_1",
-#   ".Islope0.2_80", ".Islope0.2_100", ".Islope0.4_80", ".Islope0.4_100",
-#   ".IDX", ".IDX_smooth",
-#   ".IT10_hist", ".IT5_hist",
-#   ".Itarget_base", ".Itarget_w0.8", ".Itarget_x0.2", ".Itarget_x0.8", ".Itarget_d1.2", ".Itarget_d0.8",
-#   ".ITM_hist",
-#   ".SP4010_prior", ".SP6040_prior", ".SP8040_prior",
-#   "NFref", "FMSYref", "FMSYref75"
+#   # "CC_hist20", "CC1.2", "CC1.1", "CC1.0", "CC0.9", "CC0.8", "CC0.7", "CC0.6",
+#   # ".Iratio2"
+#   # ".GB_slope6_0.66", ".GB_slope6_1", ".GB_slope8_0.66", ".GB_slope8_1",
+#   # ".Islope0.2_80", ".Islope0.2_100", ".Islope0.4_80", ".Islope0.4_100",
+#   # ".IDX", ".IDX_smooth",
+#   # ".IT10_hist", ".IT5_hist",
+#   ".Itarget_base", ".Itarget_w0.8", ".Itarget_x0.2", ".Itarget_x0.8", ".Itarget_d1.2", ".Itarget_d0.8"
+#   # ".ITM_hist",
+#   # ".SP4010_prior", ".SP6040_prior", ".SP8040_prior",
+#   # "NFref", "FMSYref", "FMSYref75"
 # )
 
+# .mp <- "CC1.0"
 fit_scenario <- function(scenario) {
   file_name <- here("generated-data", paste0(sp, "-mse-", scenario, ".rds"))
   if (!file.exists(file_name)) {
@@ -118,7 +126,11 @@ fit_scenario <- function(scenario) {
 DLMtool::setup(cpus = cores)
 mse <- map(scenarios, fit_scenario)
 snowfall::sfStop()
-for (i in seq_along(mse)) mse[[i]]@OM$RefY <- ref_catch * 1
+
+ref_catch <- mean(catch[(yrs-5+1):yrs])
+for (i in seq_along(mse)) mse[[i]]@OM$RefY <- ref_catch
+
+# plot_main_projections(mse[[2]])
 
 # Satisficing -----------------------------------------------------------------
 
@@ -129,10 +141,10 @@ pm_avg <- group_by(pm_df, MP) %>% summarise_if(is.numeric, mean)
 pm_min <- group_by(pm_df, MP) %>% summarise_if(is.numeric, min)
 saveRDS(pm_df_list, file = here("generated-data", "rex-pm-all.rds"))
 
-plot_tigure(pm_avg, satisficed = c("LT LRP" = 0.75, "STC" = 0.5))
-plot_tigure(pm_min)
+plot_tigure(pm_avg, satisficed = c("LT LRP" = 0.7, "STC" = 0.5, "LTC" = 0.2))
+plot_tigure(pm_min, satisficed = c("LT LRP" = 0.7, "STC" = 0.5, "LTC" = 0.2))
 
-mp_sat <- dplyr::filter(pm_avg, `LT LRP` > 0.5, `STC` > 0.3) %>%
+mp_sat <- dplyr::filter(pm_min, `LT LRP` > 0.7, `STC` > 0.5, `LTC` > 0.2) %>%
   # arrange(-`LT LRP`, -`LT LRP`, -`LT USR`, -`STC`, -`LTC`, -AAVC) %>%
   pull(MP)
 mp_sat <- mp_sat[!mp_sat %in% reference_mp]
@@ -169,7 +181,7 @@ plots <- gfdlm::plot_factory(
   mp_not_sat2 = mp_eg_not_sat,
   mp_ref = reference_mp,
   custom_pal = custom_pal,
-  eg_scenario = "ceq100",
+  eg_scenario = "ceq200",
   tradeoff = c("LT LRP", "STC"),
   catch_breaks = catch_breaks,
   catch_labels = catch_labels,
@@ -183,18 +195,39 @@ pm_angle <- theme(
   panel.grid.major.y = element_line(colour = "grey85"),
   panel.grid.minor.y = element_line(colour = "grey96")
 )
-plots$dot_refset + pm_angle
-# ggsave("~/Desktop/g1.pdf", width = 11, height = 7)
-plots$dot_refset_avg
-# ggsave("~/Desktop/g2.png", width = 6, height = 4)
+# plots$dot_refset + pm_angle
 
-plots$tigure_refset
-# ggsave("~/Desktop/g3.png", width = 6, height = 7)
+.ggsave("dot-refset-avg", width = 6, height = 5, plot = plots$dot_refset_avg)
 
-plots$radar_refset
-# ggsave("~/Desktop/g4.png", width = 11, height = 11)
+# plots$radar_refset_avg
+# plots$radar_refset_min
 
-plots$lollipops_refset
-# ggsave("~/Desktop/g5.png", width = 9, height = 9)
+.ggsave("tradeoff-refset", width = 7, height = 4, plot = plots$tradeoff_refset)
 
-plots$parallel_refset
+# plots$tradeoff_robset
+
+.ggsave("tigure-refset", width = 6.5, height = 8, plot = plots$tigure_refset)
+.ggsave("tigure-refset-min", width = 4, height = 7, plot = plots$tigure_refset_min)
+.ggsave("tigure-refset-avg", width = 4, height = 7, plot = plots$tigure_refset_avg)
+
+# plots$radar_refset
+# plots$lollipops_refset
+
+g <- plots$projections_index +
+  scale_x_continuous(breaks = seq(1975, 2090, 25)) +
+  coord_cartesian(ylim = c(0, 9))
+.ggsave("projections-index", width = 12, height = 8, plot = g)
+
+
+# plots$projections$`no-cpue-light`
+# plots$projections$ceq200
+# plots$projections$`high-m`
+
+walk(names(plots$projections), ~{
+  .ggsave(paste0("projections-", .x), width = 8, height = 8,
+    plot = plots$projections[[.x]])
+})
+.ggsave("projections-not-sat2", width = 6.5, height = 9, plot = plots$projections_not_sat2)
+.ggsave("projections-not-sat", width = 6.5, height = 20, plot = plots$projections_not_sat)
+.ggsave("projections-scenarios-ref", width = 8, height = 8, plot = plots$projections_scenarios)
+
