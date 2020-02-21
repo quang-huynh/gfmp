d18f59963422f565a2f34e9d672199789aeacfb8
Sean Anderson <sean@seananderson.ca>
2020-02-20 16:21:33 -0800

    Overhaul conditioning (including new data)

--- analysis/rex/02-condition-sra.R
+++ analysis/rex/02-condition-sra.R
@@ -8,7 +8,7 @@ library("ggplot2")
 
 species_name <- "Rex Sole"
 starting_year <- 1996
-ending_year <- 2018
+ending_year <- 2019
 all_years <- seq(starting_year, ending_year)
 nyear <- length(all_years)
 
@@ -16,8 +16,8 @@ fig_dir <- here("report", "figure")
 if (!dir.exists(fig_dir)) dir.create(fig_dir)
 
 drex <- readRDS(here("generated-data", "rex-filter-data.rds"))
-d_catch <- readRDS(here::here("generated-data", "rex-catch.rds"))
-drex$catch <- dplyr::filter(d_catch, year <= 2018)
+d_catch <- readRDS(here::here("generated-data", "rex-catch2.rds"))
+drex$catch <- dplyr::filter(d_catch, year >= 1996, year <= 2019)
 
 rex_om <- readRDS(here("generated-data", "rex-om.rds"))
 rex_om@M
@@ -44,16 +44,6 @@ rex_om@L5
 rex_om@LFS
 assertthat::assert_that(identical(rex_om@nyears, length(all_years)))
 
-# d_cpue <- readRDS("/Volumes/Extreme-SSD/gfs/report/data-cache/cpue-index-dat.rds")
-# table(lubridate::year(d_cpue$best_date))
-# d_cpue <- dplyr::filter(d_cpue, lubridate::year(best_date) <= 2019)
-# m <- gfsynopsis::fit_cpue_indices(dat = d_cpue,
-#   species = "rex sole", areas = "3[CD]",
-#   center = FALSE,
-#   cache = here::here("generated-data"),
-#   save_model = TRUE)
-# ggplot(m, aes(year, est)) + geom_line() + facet_wrap(~model)
-
 make_cal <- function(dat, survey, yrs, length_bin = 2) {
   cal <- dat %>%
     dplyr::filter(survey_abbrev == survey, sex == 2) %>%
@@ -73,21 +63,29 @@ catch <- drex$catch %>%
   pull(value)
 plot(all_years, catch, type = "o")
 
-# catch per unit effort from the trawl fleet only:
-# cpue <- readRDS("../../gfs/report/cpue-cache/rex-sole.rds")
-# readr::write_csv(cpue, path = here("generated-data", "rex-cpue.csv"))
-cpue <- read.csv(here("generated-data", "rex-cpue.csv")) %>%
-  filter(area == "3CD")
+# d_cpue <- readRDS("/Volumes/Extreme-SSD/gfs/report/data-cache/cpue-index-dat.rds")
+# d_cpue <- gfdata::get_cpue_index()
+# saveRDS(d_cpue, file = here::here("generated-data", "trawl-cpue-data.rds"))
+# ind <- gfsynopsis::fit_cpue_indices(dat = d_cpue,
+#   species = "rex sole", areas = "3[CD]",
+#   cache = here::here("generated-data"),
+#   year_range = c(1996, 2019),
+#   save_model = TRUE)
+# saveRDS(ind, file =  here::here("generated-data", "rex-cpue-index.rds"))
+cpue <- readRDS(here::here("generated-data", "rex-cpue-index.rds"))
+ggplot(cpue, aes(year, est)) + geom_line() + facet_wrap(~model) +
+  geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.5)
 
 indexes <- drex$survey_index %>%
   dplyr::filter(survey_abbrev %in% c("SYN WCVI")) %>%
   select(year, biomass, re) %>%
   right_join(tibble(year = all_years),by  = "year") %>%
-  left_join(rename(select(cpue, year, est, se_link), trawl_cpue = est, trawl_sd = se_link), by = "year")
+  left_join(rename(select(cpue, year, est, se_link), trawl_cpue = est, trawl_sd = se_link), by = "year") %>%
+  rename(syn_wcvi = biomass, syn_wcvi_sd = re)
 
-indexes %>% as.data.frame()
-plot(all_years, indexes$biomass, type = "p")
-plot(all_years, indexes$trawl_cpue, type = "p")
+# indexes %>% as.data.frame()
+# plot(all_years, indexes$syn_wcvi, type = "p")
+# plot(all_years, indexes$trawl_cpue, type = "p")
 
 # MSEtool::plot_composition(all_years,
 #   obs = cal_wcvi$cal,
@@ -104,64 +102,32 @@ rex_om@D <- c(0.3, 0.8) # gets replaced
 # Set up alternative OMs for reference set and robustness set -----------------
 
 saveRDS(indexes, file = "generated-data/rex-indexes.rds")
-fit_sra_rex <- function(om, c_eq = 1, ...) {
+
+fit_sra_rex_cpue <- function(om, c_eq = 1.5, ...) {
   MSEtool::SRA_scope(om,
     Chist = catch,
     C_eq = c_eq * catch[1],
-    Index = cbind(indexes$biomass, indexes$trawl_cpue),
-    I_sd = cbind(indexes$re, indexes$trawl_sd * 1.5),
-    I_type = c("1", "1"),
+    Index = cbind(indexes$syn_wcvi, indexes$trawl_cpue),
+    I_sd = cbind(indexes$syn_wcvi_sd, indexes$trawl_sd * 1.5),
+    I_type = c("B", "1"),
     cores = 1,
     drop_nonconv = TRUE, mean_fit = FALSE, ...
   )
 }
 
-rex_om@nsim <- 52
 rex_om@L5
 rex_om@LFS
 rex_om@L50
 rex_om@Linf
 om <- rex_om
-# om@L5 <- c(30, 30)
-# om@LFS <- c(35, 35)
 
-# Abookire (2006) modeled female rex sole size-at-maturity using a logistic model, obtaining a value for size at 50% maturity of 351.7 mm with a slope of 0.0392 mm-1
-# rex_sra_ceq0 <- fit_sra_rex(rex_om, c_eq = 0)
-# rex_sra_ceq50 <- fit_sra_rex(rex_om, c_eq = 0.5)
-# rex_sra_ceq100 <- fit_sra_rex(rex_om, c_eq = 1)
-rex_sra_ceq200 <- fit_sra_rex(om, c_eq = 2)
-plot(rex_sra_ceq200)
+rex_sra_ceq150 <- fit_sra_rex_cpue(om, c_eq = 1.5)
+rex_sra_ceq200 <- fit_sra_rex_cpue(om, c_eq = 2)
+# plot(rex_sra_ceq200)
+# plot(rex_sra_ceq150)
 
-# saveRDS(rex_sra_ceq0, file = here("generated-data", "rex-sra-ceq0.rds"))
-# saveRDS(rex_sra_ceq50, file = here("generated-data", "rex-sra-ceq50.rds"))
-# saveRDS(rex_sra_ceq100, file = here("generated-data", "rex-sra-ceq100.rds"))
+saveRDS(rex_sra_ceq150, file = here("generated-data", "rex-sra-ceq150.rds"))
 saveRDS(rex_sra_ceq200, file = here("generated-data", "rex-sra-ceq200.rds"))
-
-median(rex_sra_ceq0@OM@cpars$D)
-median(rex_sra_ceq50@OM@cpars$D)
-median(rex_sra_ceq100@OM@cpars$D)
-median(rex_sra_ceq200@OM@cpars$D)
-
-# Add commercial CPUE:
-reshape2::melt(indexes[,c(1, 2, 4)], id.vars = "year") %>%
-  group_by(variable) %>% mutate(value = value/max(value, na.rm=TRUE)) %>%
-  ggplot(aes(year, value, colour = variable)) + geom_point() + geom_path()
-
-#s_CAL <- array(NA, c(rex_om@nyears, length(cal_wcvi$length_bins), 2L))
-#s_CAL[,,1] <- cal_wcvi$cal
-rex_sra_ceq200_cpue <- MSEtool::SRA_scope(rex_om,
-  data = list(
-    Chist = catch,
-    Index = cbind(indexes$biomass, indexes$trawl_cpue),
-    I_sd = cbind(indexes$re, indexes$trawl_sd * 1.5),
-    I_type = c("1", "1"),
-    C_eq = 2 * catch[1]
-  ),
-  cores = 1,
-  drop_nonconv = TRUE
-)
-#plot(rex_sra_ceq200_cpue)
-saveRDS(rex_sra_ceq200_cpue, file = here("generated-data", "rex-sra-ceq200-cpue.rds"))
 
 # Alternative Reference Set OMs: M --------------------------------------------
 # Only look at higher M and time-varying M. M in BC unlikely to be lower than GOA.
@@ -169,36 +135,84 @@ saveRDS(rex_sra_ceq200_cpue, file = here("generated-data", "rex-sra-ceq200-cpue.
 rex_om@M
 rex_om_high_m <- rex_om
 rex_om_high_m@M <- c(0.35, 0.35)
-rex_sra_high_m <- fit_sra_rex(rex_om_high_m)
+rex_sra_high_m <- fit_sra_rex_cpue(rex_om_high_m, c_eq = 1.5)
 
 saveRDS(rex_sra_high_m, file = here("generated-data", "rex-sra-high-m.rds"))
-
-quantile(rex_sra_ceq50@OM@cpars$D)
-quantile(rex_sra_high_m@OM@cpars$D)
+# plot(rex_sra_high_m)
 
 # Alternative Reference Set OMs: Steepness (h) --------------------------------
 
 rex_om_low_h <- rex_om
 rex_om@h
 rex_om_low_h@h <- c(0.5, 0.7)
-rex_sra_low_h <- fit_sra_rex(rex_om_low_h)
+rex_sra_low_h <- fit_sra_rex_cpue(rex_om_low_h)
 saveRDS(rex_sra_low_h, file = here("generated-data", "rex-sra-low-h.rds"))
-
-quantile(rex_sra_ceq50@OM@cpars$D)
-quantile(rex_sra_low_h@OM@cpars$D)
-median(rex_sra_ceq50@OM@cpars$D)
-median(rex_sra_low_h@OM@cpars$D)
 
 rex_om_high_h <- rex_om
 rex_om@h
 rex_om_high_h@h <- c(0.95, 0.95)
-rex_sra_high_h <- fit_sra_rex(rex_om_high_h)
+rex_sra_high_h <- fit_sra_rex_cpue(rex_om_high_h)
 saveRDS(rex_sra_high_h, file = here("generated-data", "rex-sra-high-h.rds"))
 
-quantile(rex_sra_ceq50@OM@cpars$D)
-quantile(rex_sra_high_h@OM@cpars$D)
+# No CPUE ---------------------------------------------------------------------
 
-# Robustness Set OMs: M increasing/decreasing over time -----------------------
+fit_sra_rex_no_cpue <- function(om, c_eq = 1.5, ...) {
+  MSEtool::SRA_scope(om,
+    Chist = catch,
+    C_eq = c_eq * catch[1],
+    Index = cbind(indexes$syn_wcvi),
+    I_sd = cbind(indexes$syn_wcvi_sd),
+    I_type = c("B"),
+    cores = 1,
+    drop_nonconv = TRUE, mean_fit = FALSE, ...
+  )
+}
+rex_sra_no_cpue <- fit_sra_rex_no_cpue(rex_om, c_eq = 2)
+saveRDS(rex_sra_no_cpue, file = here("generated-data", "rex-sra-no-cpue.rds"))
+# plot(rex_sra_no_cpue)
+
+# No CPUE and lightly fished before 1996 --------------------------------------
+
+rex_sra_unfished <- fit_sra_rex_no_cpue(rex_om, c_eq = 1)
+saveRDS(rex_sra_unfished, file = here("generated-data", "rex-sra-no-cpue-light.rds"))
+# plot(rex_sra_unfished)
+
+# Growth parameters from Oregon @hosie1976 ------------------------------------
+
+om <- rex_om
+om@K <- c(0.17, 0.17)
+om@Linf <- c(37.2, 37.2)
+om@t0 <- c(-0.57, -0.57)
+rex_sra_oregon <- fit_sra_rex_cpue(om, c_eq = 2)
+# rex_sra_oregon <- fit_sra_rex_n(om, c_eq = 2)
+saveRDS(rex_sra_oregon, file = here("generated-data", "rex-sra-oregon.rds"))
+# plot(rex_sra_oregon)
+
+# Shift selectivity curve right -----------------------------------------------
+
+om <- rex_om
+om@L5
+om@LFS
+om@L50
+om@L5 <- c(22, 22)
+om@LFS <- c(30, 30)
+rex_sra_sel1 <- fit_sra_rex_cpue(om, c_eq = 1.5)
+# plot(rex_sra_sel1)
+saveRDS(rex_sra_sel1, file = here("generated-data", "rex-sra-sel1.rds"))
+
+# Shift selectivity curve left -----------------------------------------------
+
+# om <- rex_om
+# om@L5
+# om@LFS
+# om@L50
+# om@L5 <- c(14, 14)
+# om@LFS <- c(23, 23)
+# rex_sra_sel2 <- fit_sra_rex_cpue(om, c_eq = 1.5)
+# plot(rex_sra_sel2)
+# saveRDS(rex_sra_sel2, file = here("generated-data", "rex-sra-sel2.rds"))
+
+# Robustness Set OMs: M increasing over time ----------------------------------
 
 rex_om_inc_m <- rex_om
 rex_om_inc_m@nyears
@@ -209,21 +223,23 @@ m <- seq(0.2, 0.4, length.out = rex_om_inc_m@proyears)
 for (i in seq_along(m)) M_age[, , rex_om_inc_m@nyears + i] <- m[i]
 plot(M_age[1,1,]) # one sim; one age
 rex_om_inc_m@cpars$M_ageArray <- M_age
-rex_sra_inc_m <- fit_sra_rex(rex_om_inc_m)
+rex_sra_inc_m <- fit_sra_rex_cpue(rex_om_inc_m)
 saveRDS(rex_sra_inc_m, file = here("generated-data", "rex-sra-inc-m.rds"))
 
 # Set up the scenario names ---------------------------------------------------
 
 sc <- tibble::tribble(
   ~scenario,     ~scenario_human,        ~scenario_type,
-  "ceq50",       "Ceq. 50%",         "Reference",
-  "ceq100",      "Ceq. 100%",        "Reference",
+  "ceq150",      "Ceq. 150%",        "Reference",
   "ceq200",      "Ceq. 200%",        "Reference",
-  "ceq200-cpue", "Ceq. 200%, CPUE", "Reference",
   "high-m",      "M = 0.35",              "Reference",
   "low-h",       "h = 0.5-0.7",           "Reference",
   "high-h",      "h = 0.95",              "Reference",
-  "inc-m",       "M inc,",          "Robustness"
+  "sel1", "Shifted sel.", "Reference",
+  "oregon", "Oregon growth", "Reference",
+  "no-cpue",     "No CPUE",          "Reference",
+  "no-cpue-light","Lightly fished",   "Reference",
+  "inc-m",       "M inc.",          "Robustness"
 )
 sc <- mutate(sc, order = seq_len(n()))
 saveRDS(sc, file = "generated-data/rex-scenarios.rds")
@@ -293,52 +309,3 @@ sel %>%
   ylab("Selectivity") + xlab("Length") +
   coord_cartesian(expand = FALSE, ylim = c(-0.01, 1.01))
 ggsave(here::here("report/figure/rex-selectivity.png"), width = 5, height = 3)
-
-# ------------------------------------------------------------------------
-
-# fit_sra_rex <- function(om, c_eq = 1, ...) {
-#   MSEtool::SRA_scope(rex_om,
-#     Chist = catch, Index = indexes$biomass,
-#     C_eq = c_eq * catch[1],
-#     I_sd = indexes$re, I_type = "1", cores = 1,
-#     drop_nonconv = TRUE, mean_fit = FALSE, ...
-#   )
-# }
-# rex_om@nsim <- 18
-# rex_sra_ceq200 <- fit_sra_rex(rex_om, c_eq = 2)
-
-# sra_rex <- purrr::map(sc$scenario, ~ {
-#   readRDS(here("generated-data", paste0("rex-sra-", .x, ".rds")))
-# })
-# names(sra_rex) <- sc$scenario
-
-# sra <- sra_rex[[1]]
-# sra <- rex_sra_ceq200
-# OM <- sra@OM
-#
-# library(DLMtool)
-# mse <- runMSE(OM, Hist = TRUE, parallel = FALSE)
-#
-#
-#
-# library(MSEtool)
-# om <- DLMtool::testOM
-# om@nsim <- 10
-# set.seed(1)
-# m <- SRA_scope(om,
-#   data = list(Chist = runif(10), Index = runif(10), I_sd = rep(0.1, 10))
-# )
-# fits <- list()
-# fits[[1]] <- m
-# fits[[2]] <- m
-# names(fits) <- c("Sc 1", "Sc 2")
-# oms <- purrr::map(fits, "OM")
-# mse_list <- purrr::map(oms, runMSE, Hist = TRUE, parallel = FALSE)
-#
-# dim(mse_list[[1]]@TSdata$SSB)
-# mse_list[[1]]@Ref$SSBMSY
-
-# make plot_historical_ts() work with:
-# 1. a list of mse objects
-# 2. an optional `ref_pt` argument that pulls a value from `@Ref` and divides
-# the time series by it.
