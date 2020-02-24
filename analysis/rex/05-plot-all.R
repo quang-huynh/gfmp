c94b32eb1009ae8dc19d70a33ab2531c250616e7
Sean Anderson <sean@seananderson.ca>
2020-02-24 15:49:43 -0800

    Running the new rex MSE

--- analysis/rex/05-plot-all.R
+++ analysis/rex/05-plot-all.R
@@ -14,7 +14,7 @@ sp <- "rex" # Species: used in filenames
 cores <- floor(parallel::detectCores() / 2L)
 sc <- readRDS("generated-data/rex-scenarios.rds")
 sc # look good?
-nsim <- 50L
+nsim <- 75L
 interval <- 2L
 base_om <- "ceq150" # affects some default plots
 mp <- suppressMessages(readr::read_csv(here("analysis", "rex", "mp.txt"), comment = "#"))
@@ -23,7 +23,7 @@ reference_mp <- c("FMSYref75", "NFref", "FMSYref")
 ref_mp_cols <- c("grey45", "grey10", "grey75") %>% set_names(reference_mp)
 #
 catch_breaks <- seq(0, 500000, 100000)
-catch_labels <- catch_breaks/1000
+catch_labels <- catch_breaks / 1000
 
 # Satisficing rules:
 # LT_LRP_thresh <- 0.8
@@ -40,7 +40,7 @@ catch <- d_catch %>%
   pull(value)
 
 yrs <- length(catch)
-ref_aadc <- gfdlm:::get_aadc(catch[(yrs-10+1):yrs])
+ref_aadc <- gfdlm:::get_aadc(catch[(yrs - 10 + 1):yrs])
 
 `LT LRP` <- gfdlm::pm_factory("SBMSY", 0.4, c(36, 50))
 `LT USR` <- gfdlm::pm_factory("SBMSY", 0.8, c(36, 50))
@@ -87,9 +87,11 @@ om <- map(scenarios, ~ {
   .n <- nsim_cpars * (om@nyears + om@proyears)
   set.seed(om@seed)
   .e <- DLMtool::trlnorm(.n, 1,
-    cv = runif(.n, min = min(om@Iobs), max = max(om@Iobs)))
+    cv = runif(.n, min = min(om@Iobs), max = max(om@Iobs))
+  )
   om@cpars$AddIerr <- array(.e,
-    dim = c(nsim_cpars, n.ind, om@nyears + om@proyears))
+    dim = c(nsim_cpars, n.ind, om@nyears + om@proyears)
+  )
   om
 })
 
@@ -139,7 +141,8 @@ missing <- mp$mp[!map_lgl(mp$mp, exists)]
 assert_that(length(missing) == 0,
   msg = glue::glue(
     "{paste(missing, collapse = ', ')} do not exist in the current environment."
-  ))
+  )
+)
 
 fit_scenario <- function(scenario) {
   file_name <- here("generated-data", paste0(sp, "-mse-", scenario, ".rds"))
@@ -161,7 +164,7 @@ snowfall::sfStop()
 # plot_main_projections(mse[[1]])
 # plot_index(mse, type = "AddInd", omit_index_fn = oddify)
 
-ref_catch <- min(catch[(yrs-5+1):yrs])
+ref_catch <- mean(catch[(yrs - 5 + 1):yrs])
 for (i in seq_along(mse)) mse[[i]]@OM$RefY <- ref_catch
 
 # plot_main_projections(mse[[2]])
@@ -175,10 +178,10 @@ pm_avg <- group_by(pm_df, MP) %>% summarise_if(is.numeric, mean)
 pm_min <- group_by(pm_df, MP) %>% summarise_if(is.numeric, min)
 saveRDS(pm_df_list, file = here("generated-data", "rex-pm-all.rds"))
 
-plot_tigure(pm_avg, satisficed = c("LT LRP" = 0.7, "STC" = 0.5, "LTC" = 0.2))
-plot_tigure(pm_min, satisficed = c("LT LRP" = 0.7, "STC" = 0.5, "LTC" = 0.2))
+plot_tigure(pm_avg, satisficed = c("LT LRP" = 0.7, "STC" = 0.6))
+plot_tigure(pm_min, satisficed = c("LT LRP" = 0.7, "STC" = 0.6))
 
-mp_sat <- dplyr::filter(pm_min, `LT LRP` > 0.7, `STC` > 0.5, `LTC` > 0.2) %>%
+mp_sat <- dplyr::filter(pm_avg, `LT LRP` > 0.7, `STC` > 0.6) %>%
   # arrange(-`LT LRP`, -`LT LRP`, -`LT USR`, -`STC`, -`LTC`, -AAVC) %>%
   pull(MP)
 mp_sat <- mp_sat[!mp_sat %in% reference_mp]
@@ -192,15 +195,14 @@ stopifnot(length(mp_not_sat) > 1)
 custom_pal <- c(RColorBrewer::brewer.pal(8, "Set2")[seq_along(mp_sat)], ref_mp_cols) %>%
   set_names(mp_sat_with_ref)
 
-satisficed_criteria <- c("LT LRP" = 0.7, "STC" = 0.5, "LTC" = 0.2)
+satisficed_criteria <- c("LT LRP" = 0.7, "STC" = 0.6)
 
 mp_eg_not_sat <- c(
   "CC_hist20",
-  "CC1.0",
+  "CC0.8",
   ".GB_slope8_0.66",
   ".Islope0.2_80",
-  ".ICI2",
-  ".IDX_smooth",
+  ".IDX",
   ".IT5_hist",
   ".ITM_hist",
   ".SP6040_prior"
@@ -215,84 +217,91 @@ plots <- gfdlm::plot_factory(
   mp_not_sat2 = mp_eg_not_sat,
   mp_ref = reference_mp,
   custom_pal = custom_pal,
-  eg_scenario = "ceq200",
+  eg_scenario = "ceq150",
   tradeoff = c("LT LRP", "STC"),
   catch_breaks = catch_breaks,
   catch_labels = catch_labels,
   satisficed_criteria = satisficed_criteria,
   skip_projections = FALSE,
-  dodge = 0.8
+  survey_type = "AddInd",
+  omit_index_fn = function(x) seq(2, x, by = 2)
 )
 
-pm_angle <- theme(
-  axis.text.x.bottom = element_text(angle = 90, hjust = 1),
-  panel.grid.major.y = element_line(colour = "grey85"),
-  panel.grid.minor.y = element_line(colour = "grey96")
-)
+# pm_angle <- theme(
+#   axis.text.x.bottom = element_text(angle = 90, hjust = 1),
+#   panel.grid.major.y = element_line(colour = "grey85"),
+#   panel.grid.minor.y = element_line(colour = "grey96")
+# )
 # plots$dot_refset + pm_angle
 
-.ggsave("dot-refset-avg", width = 6, height = 5, plot = plots$dot_refset_avg)
+.ggsave("dot-refset-avg", width = 7.5, height = 4.5, plot = plots$dot_refset_avg)
 
-# plots$radar_refset_avg
-# plots$radar_refset_min
-
-.ggsave("tradeoff-refset", width = 7, height = 4, plot = plots$tradeoff_refset)
-
-# plots$tradeoff_robset
+g <- plots$tradeoff_refset + facet_wrap(~scenario, ncol = 3)
+.ggsave("tradeoff-refset", width = 7.5, height = 5, plot = g)
+.ggsave("tradeoff-robset", width = 6, height = 3, plot = plots$tradeoff_robset)
 
 .ggsave("tigure-refset", width = 6.5, height = 8, plot = plots$tigure_refset)
+.ggsave("tigure-robset", width = 4, height = 3.5, plot = plots$tigure_robset)
 .ggsave("tigure-refset-min", width = 4, height = 7, plot = plots$tigure_refset_min)
 .ggsave("tigure-refset-avg", width = 4, height = 7, plot = plots$tigure_refset_avg)
 
-# plots$radar_refset
-# plots$lollipops_refset
+.ggsave("radar-refset", width = 10, height = 10, plot = plots$radar_refset)
+.ggsave("radar-refset-avg", width = 6, height = 6, plot = plots$radar_refset_avg)
 
 g <- plots$projections_index +
-  scale_x_continuous(breaks = seq(1975, 2090, 25)) +
-  coord_cartesian(ylim = c(0, 9))
-.ggsave("projections-index", width = 12, height = 8, plot = g)
+  scale_x_continuous(breaks = seq(1980, 2090, 20)) +
+  coord_cartesian(ylim = c(0, 15e6)) +
+  scale_y_continuous(labels = function(x) x / 1e6)
+.ggsave("projections-index", width = 12, height = 10, plot = g)
 
-# plots$projections$`no-cpue-light`
-plots$projections$`inc-m`
-# plots$projections$ceq200
-# plots$projections$`high-m`
-
-walk(names(plots$projections), ~{
-  .ggsave(paste0("projections-", .x), width = 8, height = 8,
-    plot = plots$projections[[.x]])
+walk(names(plots$projections), ~ {
+  .ggsave(paste0("projections-", .x),
+    width = 8, height = 10,
+    plot = plots$projections[[.x]]
+  )
 })
-.ggsave("projections-not-sat2", width = 6.5, height = 9, plot = plots$projections_not_sat2)
-.ggsave("projections-not-sat", width = 6.5, height = 20, plot = plots$projections_not_sat)
-.ggsave("projections-scenarios-ref", width = 8, height = 8, plot = plots$projections_scenarios)
+.ggsave("projections-not-sat2",
+  width = 6.5, height = 9,
+  plot = plots$projections_not_sat2
+)
+.ggsave("projections-not-sat",
+  width = 6.5, height = 20,
+  plot = plots$projections_not_sat
+)
+.ggsave("projections-scenarios-ref",
+  width = 8, height = 10,
+  plot = plots$projections_scenarios
+)
 
-x <- purrr::map(scenarios_ref,
-  ~ DLMtool::Sub(mse[[.x]], MPs = mp_sat_with_ref)) %>%
+get_filtered_scenario <- function(type, column) {
+  dplyr::filter(sc, scenario_type == type) %>%
+    dplyr::pull(!!column) %>%
+    purrr::set_names()
+}
+scenarios <- sc$scenario %>% set_names()
+scenarios_human <- sc$scenario_human %>% set_names()
+scenarios_ref <- get_filtered_scenario("Reference", "scenario")
+scenarios_ref_human <- get_filtered_scenario("Reference", "scenario_human")
+x <- purrr::map(
+  scenarios_ref,
+  ~ DLMtool::Sub(mse[[.x]], MPs = mp_sat_with_ref)
+) %>%
   set_names(scenarios_ref_human)
 
-plot_index(x, type = "AddInd")
+plot_index(x, type = "AddInd", omit_index_fn = oddify)
 
 g <- c("Ceq. 150%", "Ceq. 200%", "Lightly fished", "No CPUE") %>%
   set_names() %>%
-  map(~x[[.]]) %>%
+  map(~ x[[.]]) %>%
   plot_scenario_projections()
-.ggsave("projections-scenarios-ref1", width = 8, height = 8)
+.ggsave("projections-scenarios-ref1", width = 8, height = 10)
 
-g <- c("Ceq. 150%", "M = 0.35", "h = 0.5-0.7" , "h = 0.95") %>%
+g <- c("Ceq. 150%", "M = 0.35", "h = 0.95", "Lower selectivity") %>%
   set_names() %>%
-  map(~x[[.]]) %>%
+  map(~ x[[.]]) %>%
   plot_scenario_projections()
-.ggsave("projections-scenarios-ref2", width = 8, height = 8)
+.ggsave("projections-scenarios-ref2", width = 8, height = 10)
 
-g <- c("Ceq. 150%", "Shifted sel.", "Oregon growth" , "Ceq. 200%") %>%
-  set_names() %>%
-  map(~x[[.]]) %>%
-  plot_scenario_projections()
-.ggsave("projections-scenarios-ref2", width = 8, height = 8)
-
-x <- purrr::map("inc-m",
-  ~ DLMtool::Sub(mse[[.x]], MPs = mp_sat_with_ref)) %>%
-  set_names("inc-m")
-
-plot_projection_ts(x[[length(x)]])
-# plot_projection_ts(x[[1]])
-plot_main_projections(x[[length(x)]])
+.ggsave("worms-proj", width = 13, height = 8.5, plot = plots$worms_proj)
+.ggsave("worms-hist-proj", width = 13, height = 8.5, plot = plots$worms_hist_proj)
+.ggsave("kobe", width = 13, height = 8.5, plot = plots$kobe)
