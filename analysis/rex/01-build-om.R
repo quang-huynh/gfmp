271e67bbd2fdb455ce4a5892bb88e4a23ddc4766
Sean Anderson <sean@seananderson.ca>
2020-03-02 10:19:27 -0800

    Cleanup

--- analysis/rex/01-build-om.R
+++ analysis/rex/01-build-om.R
@@ -5,12 +5,11 @@ library(DLMtool)
 library(MSEtool)
 library(dplyr)
 library(ggplot2)
-filter <- dplyr::filter # Chris sometimes has problems. #So does Robyn :-D
+setwd(here::here())
 
 source_rmd <- function(rmd_file){
   knitr::knit(rmd_file, output = tempfile())
 }
-
 rex_om_rmd <- list.files("report", pattern = "om-rex\\.Rmd",
   full.names = TRUE)
 source_rmd(rex_om_rmd)
